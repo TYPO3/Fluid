5cb6fc5b903bd878aedf156dbaaa42c2ea2b020e
Karsten Dambekalns <karsten@typo3.org>
2009-04-16 04:32:39 +0000

    Fluid: * validateArguments() in the AbstractViewHelper now accepts anything \Traversable as "array"

--- Classes/Core/AbstractViewHelper.php
+++ Classes/Core/AbstractViewHelper.php
@@ -213,7 +213,7 @@ abstract class AbstractViewHelper implements \F3\Fluid\Core\ViewHelperInterface 
 				if ($this->arguments[$argumentName] === $registeredArgument->getDefaultValue()) continue;
 
 				if ($type === 'array') {
-					if (!is_array($this->arguments[$argumentName]) && !$this->arguments[$argumentName] instanceof \ArrayAccess) {
+					if (!is_array($this->arguments[$argumentName]) && !$this->arguments[$argumentName] instanceof \ArrayAccess && !$this->arguments[$argumentName] instanceof \Traversable) {
 						throw new \F3\Fluid\Core\RuntimeException('The argument "' . $argumentName . '" was registered with type array, but is of type ' . gettype($this->arguments[$argumentName]), 1237900529);
 					}
 				} else {
