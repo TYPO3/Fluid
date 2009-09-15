aa0794325cdece0e38aa22cdab75a8199c18f216
Sebastian Kurfürst <sebastian@typo3.org>
2009-09-15 15:16:36 +0000

    [!!!][~FEATURE] Fluid (ViewHelpers): Improved the way flash messages are handled. Now there is a separate "FlashMessages" object which encapsulates handling the flash messages. This removes $this->pushFlashMessage and $this->popFlashMessages inside the Controller and {flashMessages} inside Fluid Template. PLEASE UPGRADE by replacing "$this->pushFlashMessage(" with "$this->flashMessages->add(" inside your Controllers, and use <f:renderFlashMessages /> instead of the custom for loop inside your templates. Note: You can set a custom CSS class for outputting flash messages by using <f:renderFlashMessages class="..." /> inside your templates. Resolves #4638.

---
+++ Classes/ViewHelpers/RenderFlashMessagesViewHelper.php
@@ -0,0 +1,90 @@
+<?php
+declare(ENCODING = 'utf-8');
+namespace F3\Fluid\ViewHelpers;
+
+/*                                                                        *
+ * This script belongs to the FLOW3 package "Fluid".                      *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU Lesser General Public License as published by the *
+ * Free Software Foundation, either version 3 of the License, or (at your *
+ * option) any later version.                                             *
+ *                                                                        *
+ * This script is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHAN-    *
+ * TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser       *
+ * General Public License for more details.                               *
+ *                                                                        *
+ * You should have received a copy of the GNU Lesser General Public       *
+ * License along with the script.                                         *
+ * If not, see http://www.gnu.org/licenses/lgpl.html                      *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+/**
+ * View helper which renders the flash messages (if there are any) as an unsorted list.
+ *
+ * In case you need custom Flash Message HTML output, please write your own ViewHelper for the moment.
+ *
+ *
+ * = Examples =
+ *
+ * <code title="Simple">
+ * <f:renderFlashMessages />
+ * </code>
+ * Renders an ul-list of flash messages.
+ *
+ * <code title="Output with css class">
+ * <f:renderFlashMessages class="specialClass" />
+ * </code>
+ *
+ * Output:
+ * <ul class="specialClass">
+ *  ...
+ * </ul>
+ *
+ * @version $Id: ForViewHelper.php 2914 2009-07-28 18:26:38Z bwaidelich $
+ * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License, version 3 or later
+ * @scope prototype
+ */
+class RenderFlashMessagesViewHelper extends \F3\Fluid\Core\ViewHelper\TagBasedViewHelper {
+
+	/**
+	 * @var string
+	 */
+	protected $tagName = 'ul';
+
+	/**
+	 * Initialize arguments
+	 *
+	 * @return void
+	 * @author Sebastian Kurfürst <sbastian@typo3.org>
+	 * @api
+	 */
+	public function initializeArguments() {
+		$this->registerUniversalTagAttributes();
+	}
+
+	/**
+	 * Render method.
+	 *
+	 * @return string rendered Flash Messages, if there are any.
+	 * @author Sebastian Kurfürst <sbastian@typo3.org>
+	 * @api
+	 */
+	public function render() {
+		$flashMessages = $this->controllerContext->getFlashMessages()->getAllAndFlush();
+		if (count($flashMessages) > 0) {
+			$tagContent = '';
+			foreach ($flashMessages as $singleFlashMessage) {
+				$tagContent .=  '<li>' . htmlspecialchars($singleFlashMessage) . '</li>';
+			}
+			$this->tag->setContent($tagContent);
+			return $this->tag->render();
+		}
+		return '';
+	}
+}
+
+?>
