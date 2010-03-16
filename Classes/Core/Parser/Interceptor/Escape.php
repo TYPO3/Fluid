618b0d7d03cd77bbec6753aaccea936e15ae64bd
Sebastian Kurfürst <sebastian@typo3.org>
2010-03-16 08:08:17 +0000

    [+BUGFIX] Fluid (Core): Removed debug output. Sorry!

--- Classes/Core/Parser/Interceptor/Escape.php
+++ Classes/Core/Parser/Interceptor/Escape.php
@@ -68,7 +68,6 @@ class Escape implements \F3\Fluid\Core\Parser\InterceptorInterface {
 	public function process(\F3\Fluid\Core\Parser\SyntaxTree\NodeInterface $node, $interceptorPosition) {
 		if ($interceptorPosition === \F3\Fluid\Core\Parser\InterceptorInterface::INTERCEPT_OPENING_VIEWHELPER) {
 			if (!$node->getViewHelper()->isEscapingInterceptorEnabled()) {
-				echo "JAA";
 				$this->interceptorEnabled = FALSE;
 				$this->viewHelperNodesWhichDisableTheInterceptor[] = $node;
 			}
