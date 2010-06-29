6173d215ab77e48a261345a37c669e838ef6b8ff
Sebastian Kurfürst <sebastian@typo3.org>
2010-06-29 17:16:43 +0000

    [~TASK] Fluid (ViewHelpers): Refactored f:persistence.identity ViewHelper to f:identity.json ViewHelper, giving the identity of an object as JSON string.

---
+++ Classes/ViewHelpers/Identity/JsonViewHelper.php
@@ -0,0 +1,86 @@
+<?php
+declare(ENCODING = 'utf-8');
+namespace F3\Fluid\ViewHelpers\Identity;
+
+/*                                                                        *
+ * This script belongs to the FLOW3 package "Fluid".                      *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU Lesser General Public License as published by the *
+ * Free Software Foundation, either version 3 of the License, or (at your *
+ * option) any later version.                                             *
+ *                                                                        *
+ * This script is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHAN-    *
+ * TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser       *
+ * General Public License for more details.                               *
+ *                                                                        *
+ * You should have received a copy of the GNU Lesser General Public       *
+ * License along with the script.                                         *
+ * If not, see http://www.gnu.org/licenses/lgpl.html                      *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+/**
+ * Renders the identity of a persisted object (if it has an identity).
+ * Useful for using the identity outside of the form view helpers
+ * (e.g. JavaScript and AJAX).
+ *
+ * = Examples =
+ *
+ * <code title="Single alias">
+ * <f:persistence.identity object="{post.blog}" />
+ * </code>
+ * <output>
+ * 97e7e90a-413c-44ef-b2d0-ddfa4387b5ca
+ * </output>
+ *
+ * @version $Id$
+ * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License, version 3 or later
+ * @api
+ * @scope prototype
+ */
+class JsonViewHelper extends \F3\Fluid\Core\ViewHelper\AbstractViewHelper {
+
+	/**
+	 * @var \F3\FLOW3\Persistence\PersistenceManagerInterface
+	 */
+	protected $persistenceManager;
+
+	/**
+	 * Injects the FLOW3 Persistence Manager
+	 *
+	 * @param \F3\FLOW3\Persistence\PersistenceManagerInterface $persistenceManager
+	 * @return void
+	 * @author Christopher Hlubek <hlubek@networkteam.com>
+	 */
+	public function injectPersistenceManager(\F3\FLOW3\Persistence\PersistenceManagerInterface $persistenceManager) {
+		$this->persistenceManager = $persistenceManager;
+	}
+
+	/**
+	 * Renders the output of this view helper
+	 *
+	 * @param object $object The persisted object
+	 * @return string Identity
+	 * @author Christopher Hlubek <hlubek@networkteam.com>
+	 * @api
+	 */
+	public function render($object = NULL) {
+		if ($object === NULL) {
+			$object = $this->renderChildren();
+		}
+		if (!is_object($object)) {
+			throw new \F3\Fluid\Exception('f:identity.json expects an object, ' . \gettype($object) . ' given.', 1277830439);
+		}
+		$identifier = $this->persistenceManager->getIdentifierByObject($object);
+		if ($identifier === NULL) {
+			return '{}';
+		} else {
+			return json_encode(array('__identity' => $identifier));
+		}
+	}
+}
+
+?>
