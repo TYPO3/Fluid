2742ab6018a5da1fddb5f18a07ceee4782e1b554
Bastian Waidelich <bastian@typo3.org>
2010-07-22 14:46:40 +0000

    [+FEATURE] Fluid (ViewHelpers): Added count ViewHelper (slightly improved forwardport from fluid extension). This resolves #8992

---
+++ Classes/ViewHelpers/CountViewHelper.php
@@ -0,0 +1,60 @@
+<?php
+declare(ENCODING = 'utf-8');
+namespace F3\Fluid\ViewHelpers;
+
+/*                                                                        *
+ * This script belongs to the FLOW3 package "Fluid".                      *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU Lesser General Public License as published by the *
+ * Free Software Foundation, either version 3 of the License, or (at your *
+ * option) any later version.                                             *
+ *                                                                        *
+ * This script is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHAN-    *
+ * TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser       *
+ * General Public License for more details.                               *
+ *                                                                        *
+ * You should have received a copy of the GNU Lesser General Public       *
+ * License along with the script.                                         *
+ * If not, see http://www.gnu.org/licenses/lgpl.html                      *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+/**
+ * This ViewHelper counts elements of the specified array or countable object.
+ *
+ * = Examples =
+ *
+ * <code title="Count array elements">
+ * <f:count subject="{0:1, 1:2, 2:3, 3:4}" />
+ * </code>
+ * <output>
+ * 4
+ * </output>
+ *
+ * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License, version 3 or later
+ * @api
+ * @scope prototype
+ */
+class CountViewHelper extends \F3\Fluid\Core\ViewHelper\AbstractViewHelper {
+
+	/**
+	 * Counts the items of a given property.
+	 *
+	 * @param array $subject The array or ObjectStorage to iterated over
+	 * @return integer The number of elements
+	 * @author Jochen Rau <jochen.rau@typoplanet.de>
+	 * @author Bastian Waidelich <bastian@typo3.org>
+	 * @api
+	 */
+	public function render($subject) {
+		if (is_object($subject) && !$subject instanceof \Countable) {
+			throw new \F3\Fluid\Core\ViewHelper\Exception('CountViewHelper only supports arrays and objects implementing \Countable interface', 1279808078);
+		}
+		return count($subject);
+	}
+}
+
+?>
---
+++ Tests/Unit/ViewHelpers/CountViewHelperTest.php
@@ -0,0 +1,98 @@
+<?php
+declare(ENCODING = 'utf-8');
+namespace F3\Fluid\ViewHelpers;
+
+/*                                                                        *
+ * This script belongs to the FLOW3 package "Fluid".                      *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU Lesser General Public License as published by the *
+ * Free Software Foundation, either version 3 of the License, or (at your *
+ * option) any later version.                                             *
+ *                                                                        *
+ * This script is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHAN-    *
+ * TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser       *
+ * General Public License for more details.                               *
+ *                                                                        *
+ * You should have received a copy of the GNU Lesser General Public       *
+ * License along with the script.                                         *
+ * If not, see http://www.gnu.org/licenses/lgpl.html                      *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+require_once(__DIR__ . '/ViewHelperBaseTestcase.php');
+
+/**
+ * Testcase for CountViewHelper
+ *
+ * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License, version 3 or later
+ */
+class CountViewHelperTest extends \F3\Fluid\ViewHelpers\ViewHelperBaseTestcase {
+
+	/**
+	 * var \F3\Fluid\ViewHelpers\CountViewHelper
+	 */
+	protected $viewHelper;
+
+	public function setUp() {
+		parent::setUp();
+		$this->viewHelper = new \F3\Fluid\ViewHelpers\CountViewHelper();
+		$this->injectDependenciesIntoViewHelper($this->viewHelper);
+		$this->viewHelper->initializeArguments();
+	}
+
+	/**
+	 * @test
+	 * @author Bastian Waidelich <bastian@typo3.org>
+	 */
+	public function renderReturnsNumberOfElementsInAnArray() {
+		$expectedResult = 3;
+		$actualResult = $this->viewHelper->render(array('foo', 'bar', 'Baz'));
+		$this->assertSame($expectedResult, $actualResult);
+	}
+
+	/**
+	 * @test
+	 * @author Bastian Waidelich <bastian@typo3.org>
+	 */
+	public function renderReturnsNumberOfElementsInAnArrayObject() {
+		$expectedResult = 2;
+		$actualResult = $this->viewHelper->render(new \ArrayObject(array('foo', 'bar')));
+		$this->assertSame($expectedResult, $actualResult);
+	}
+
+	/**
+	 * @test
+	 * @author Bastian Waidelich <bastian@typo3.org>
+	 */
+	public function renderReturnsZeroIfGivenArrayIsEmpty() {
+		$expectedResult = 0;
+		$actualResult = $this->viewHelper->render(array());
+		$this->assertSame($expectedResult, $actualResult);
+	}
+
+	/**
+	 * @test
+	 * @author Bastian Waidelich <bastian@typo3.org>
+	 */
+	public function renderReturnsZeroIfGivenArrayIsNull() {
+		$expectedResult = 0;
+		$actualResult = $this->viewHelper->render(NULL);
+		$this->assertSame($expectedResult, $actualResult);
+	}
+
+	/**
+	 * @test
+	 * @expectedException \F3\Fluid\Core\ViewHelper\Exception
+	 * @author Bastian Waidelich <bastian@typo3.org>
+	 */
+	public function renderThrowsExceptionIfGivenSubjectIsNotCountable() {
+		$object = new \stdClass();
+		$this->viewHelper->render($object);
+	}
+
+}
+
+?>
