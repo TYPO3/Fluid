8762cd91d138adb7a4cfa4d289e5bfa2a735155a
Bastian Waidelich <bastian@typo3.org>
2011-09-06 11:58:49 +0200

    [BUGFIX] Sanitize identifier when compiling templates
    The "identifier" of a compiled template is based on the
    last modification timestamp and on the name of the original
    template/partial/layout.
    if that name contains special characters (like dashes),
    the class name of the compiled template is invalid
    resulting in a fatal error.
    This is fixed by sanitizing the identifier before creating
    the compiled template.
    
    Change-Id: I89eb9fae476d7e642618639f56d401158f69f7e8
    Resolves: #29457

--- Classes/Core/Compiler/TemplateCompiler.php
+++ Classes/Core/Compiler/TemplateCompiler.php
@@ -57,6 +57,7 @@ class TemplateCompiler {
 	 * @return boolean
 	 */
 	public function has($identifier) {
+		$identifier = $this->sanitizeIdentifier($identifier);
 		return $this->templateCache->has($identifier);
 	}
 
@@ -65,6 +66,7 @@ class TemplateCompiler {
 	 * @return \TYPO3\Fluid\Core\Parser\ParsedTemplateInterface
 	 */
 	public function get($identifier) {
+		$identifier = $this->sanitizeIdentifier($identifier);
 		if (!isset($this->syntaxTreeInstanceCache[$identifier])) {
 			$this->templateCache->requireOnce($identifier);
 			$templateClassName = 'FluidCache_' . $identifier;
@@ -80,6 +82,7 @@ class TemplateCompiler {
 	 * @return void
 	 */
 	public function store($identifier, \TYPO3\Fluid\Core\Parser\ParsingState $parsingState) {
+		$identifier = $this->sanitizeIdentifier($identifier);
 		$this->variableCounter = 0;
 		$generatedRenderFunctions = '';
 
@@ -122,6 +125,17 @@ EOD;
 				($parsingState->hasLayout() ? 'TRUE' : 'FALSE'),
 				$generatedRenderFunctions);
 		$this->templateCache->set($identifier, $templateCode);
+	}
+
+	/**
+	 * Replaces special characters by underscores
+	 * @see http://www.php.net/manual/en/language.variables.basics.php
+	 *
+	 * @param string $identifier
+	 * @return string the sanitized identifier
+	 */
+	protected function sanitizeIdentifier($identifier) {
+		return preg_replace('([^a-zA-Z0-9_\x7f-\xff])', '_', $identifier);
 	}
 
 	protected function generateCodeForSection($converted, $expectedFunctionName, $comment) {
