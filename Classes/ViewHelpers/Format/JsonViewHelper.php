87ea437e05eb0838afa45c24722785f7c0630240
Adrian Föder <adrian@foeder.de>
2012-01-27 14:31:52 +0100

    [FEATURE] Adds a format.json ViewHelper
    Provide a ViewHelper that enables putting JSON out of an array or
    object. Basically wraps PHP's native json_encode() method.
    
    Change-Id: I4bb364220e9c8a96c8ec748eed8247b7f990b72e
    Resolves: #12442
    Releases: 1.2

---
+++ Classes/ViewHelpers/Format/JsonViewHelper.php
@@ -0,0 +1,65 @@
+<?php
+namespace TYPO3\Fluid\ViewHelpers\Format;
+
+/*                                                                        *
+ * This script belongs to the FLOW3 package "Fluid".                      *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU Lesser General Public License, either version 3   *
+ *  of the License, or (at your option) any later version.                *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+
+/**
+ * Wrapper for PHPs json_encode function.
+ *
+ * = Examples =
+ *
+ * <code title="encoding a view variable">
+ * {someArray -> f:format.json()}
+ * </code>
+ * <output>
+ * ["array","values"]
+ * // depending on the value of {someArray}
+ * </output>
+ *
+ * <code title="associative array">
+ * {f:format.json(value: {foo: 'bar', bar: 'baz'})}
+ * </code>
+ * <output>
+ * {"foo":"bar","bar":"baz"}
+ * </output>
+ *
+ * <code title="non-associative array with forced object">
+ * {f:format.json(value: {0: 'bar', 1: 'baz'}, forceObject: 1)}
+ * </code>
+ * <output>
+ * {"0":"bar","1":"baz"}
+ * </output>
+ *
+ * @api
+ */
+class JsonViewHelper extends \TYPO3\Fluid\Core\ViewHelper\AbstractViewHelper {
+
+	/**
+	 * Outputs content with its JSON representation
+	 *
+	 * If $forceObject is TRUE a JSON object is outputted even if the value is a non-associative array
+	 * Example: array('foo', 'bar') as input will not be ["foo","bar"] but {"0":"foo","1":"bar"}
+	 *
+	 * @param mixed $value The incoming data to convert, or NULL if VH children should be used
+	 * @param bool $forceObject Outputs an JSON object rather than an array
+	 * @return string the JSON-encoded string.
+	 * @see http://www.php.net/manual/en/function.json-encode.php
+	 * @api
+	 */
+	public function render($value = NULL, $forceObject = FALSE) {
+		if ($value === NULL) {
+			$value = $this->renderChildren();
+		}
+		return json_encode($value, $forceObject ? JSON_FORCE_OBJECT : NULL);
+	}
+}
+?>
---
+++ Tests/Unit/ViewHelpers/Format/JsonViewHelperTest.php
@@ -0,0 +1,69 @@
+<?php
+namespace TYPO3\Fluid\Tests\Unit\ViewHelpers\Format;
+
+/*                                                                        *
+ * This script belongs to the FLOW3 package "Fluid".                      *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU General Public License, either version 3 of the   *
+ * License, or (at your option) any later version.                        *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+/**
+ */
+class JsonViewHelperTest extends \TYPO3\FLOW3\Tests\UnitTestCase {
+
+	/**
+	 * @var \TYPO3\Fluid\ViewHelpers\Format\JsonViewHelper
+	 */
+	protected $mockViewHelper;
+
+	/**
+	 */
+	protected function setUp() {
+		parent::setUp();
+		$this->mockViewHelper = $this->getMock('TYPO3\Fluid\ViewHelpers\Format\JsonViewHelper', array('renderChildren'));
+	}
+
+	/**
+	 * @test
+	 */
+	public function viewHelperConvertsSimpleAssociativeArrayGivenAsChildren() {
+		$this->mockViewHelper
+				->expects($this->once())
+				->method('renderChildren')
+				->will($this->returnValue(array('foo' => 'bar')));
+
+		$actualResult = $this->mockViewHelper->render();
+		$this->assertEquals('{"foo":"bar"}', $actualResult);
+	}
+
+	/**
+	 * @test
+	 */
+	public function viewHelperConvertsSimpleAssociativeArrayGivenAsDataArgument() {
+		$this->mockViewHelper
+				->expects($this->never())
+				->method('renderChildren');
+
+		$actualResult = $this->mockViewHelper->render(array('foo' => 'bar'));
+		$this->assertEquals('{"foo":"bar"}', $actualResult);
+	}
+
+	/**
+	 * @test
+	 */
+	public function viewHelperOutputsArrayOnIndexedArrayInputAndObjectIfSetSo() {
+		$this->mockViewHelper
+				->expects($this->any())
+				->method('renderChildren')
+				->will($this->returnValue(array('foo', 'bar', 42)));
+
+		$this->assertEquals('["foo","bar",42]', $this->mockViewHelper->render());
+		$this->assertEquals('{"0":"foo","1":"bar","2":42}', $this->mockViewHelper->render(NULL, TRUE));
+	}
+
+}
+?>
