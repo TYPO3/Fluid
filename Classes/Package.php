c523847f67b86ff5c49abe04371a46b724f976a9
Karsten Dambekalns <karsten@typo3.org>
2012-03-15 11:32:01 +0100

    [TASK] Fixes code style
    Change-Id: I20c1c7b45931b27b05e874030d044bdcdc033fb2
    Related: #34875
    Releases: 1.1

--- Classes/Package.php
+++ Classes/Package.php
@@ -11,7 +11,7 @@ namespace TYPO3\Fluid;
  * The TYPO3 project - inspiring people to share!                         *
  *                                                                        */
 
-use \TYPO3\FLOW3\Package\Package as BasePackage;
+use TYPO3\FLOW3\Package\Package as BasePackage;
 
 /**
  * The Fluid Package
