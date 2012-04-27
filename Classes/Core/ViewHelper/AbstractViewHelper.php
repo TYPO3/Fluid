5435a8bd6da5d32ee1dc95fdec7fad06019168d6
Sebastian Kurfuerst <sebastian@typo3.org>
2012-04-27 06:23:13 +0200

    [TASK] adjustments to refactored FLOW3 Context
    As the Context is now an object and not a string anymore, we adjust
    our context checks appropriately.
    
    Related: #33581
    Releases: 1.1
    Change-Id: Idcf0a2560e459d9d004d9732cea9568d264f7637

--- Classes/Core/ViewHelper/AbstractViewHelper.php
+++ Classes/Core/ViewHelper/AbstractViewHelper.php
@@ -260,7 +260,7 @@ abstract class AbstractViewHelper {
 		try {
 			return call_user_func_array(array($this, 'render'), $renderMethodParameters);
 		} catch (\TYPO3\Fluid\Core\ViewHelper\Exception $exception) {
-			if ($this->objectManager->getContext() === 'Development') {
+			if ($this->objectManager->getContext()->isDevelopment()) {
 				throw $exception;
 			} else {
 				$this->systemLogger->log('An Exception was captured: '. $exception->getMessage() . '(' . $exception->getCode() . ')', LOG_ERR, 'TYPO3.Fluid', get_class($this));
