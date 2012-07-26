2ed20916d219fa7a885ba6f9b65ae7265ed59dda
Bastian Waidelich <bastian@typo3.org>
2012-07-26 13:58:20 +0200

    [BUGFIX] Make TextareaViewHelper HTML5 aware
    This makes the arguments "rows" and "cols" of the form.textarea
    ViewHelper optional.
    
    Change-Id: Ia94a66ab4ee33e75aa789e040d9746dde2191fff
    Fixes: #31189
    Releases: 1.1, 1.2

--- Classes/ViewHelpers/Form/TextareaViewHelper.php
+++ Classes/ViewHelpers/Form/TextareaViewHelper.php
@@ -42,8 +42,8 @@ class TextareaViewHelper extends \TYPO3\Fluid\ViewHelpers\Form\AbstractFormField
 	 */
 	public function initializeArguments() {
 		parent::initializeArguments();
-		$this->registerTagAttribute('rows', 'int', 'The number of rows of a text area', TRUE);
-		$this->registerTagAttribute('cols', 'int', 'The number of columns of a text area', TRUE);
+		$this->registerTagAttribute('rows', 'int', 'The number of rows of a text area');
+		$this->registerTagAttribute('cols', 'int', 'The number of columns of a text area');
 		$this->registerTagAttribute('disabled', 'string', 'Specifies that the input element should be disabled when the page loads');
 		$this->registerArgument('errorClass', 'string', 'CSS class to set if there are errors for this view helper', FALSE, 'f3-form-error');
 		$this->registerUniversalTagAttributes();
