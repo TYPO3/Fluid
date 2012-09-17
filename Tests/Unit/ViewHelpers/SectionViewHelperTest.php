f20e61d31917e74d19e2b7fdd84a4dbf95c85d38
Karsten Dambekalns <karsten@typo3.org>
2012-09-17 11:07:45 +0200

    [TASK] Whitespace fix in class docblock
    Change-Id: I99ff5228799db2e5e84d8d89c9534ac76e7e2724
    Releases: 1.2

--- Tests/Unit/ViewHelpers/SectionViewHelperTest.php
+++ Tests/Unit/ViewHelpers/SectionViewHelperTest.php
@@ -6,7 +6,7 @@ namespace TYPO3\Fluid\Tests\Unit\ViewHelpers;
  *                                                                        *
  * It is free software; you can redistribute it and/or modify it under    *
  * the terms of the GNU Lesser General Public License, either version 3   *
- *  of the License, or (at your option) any later version.                *
+ * of the License, or (at your option) any later version.                 *
  *                                                                        *
  * The TYPO3 project - inspiring people to share!                         *
  *                                                                        */
@@ -22,16 +22,16 @@ class SectionViewHelperTest extends \TYPO3\FLOW3\Tests\UnitTestCase {
 	 */
 	public function sectionIsAddedToParseVariableContainer() {
 		$section = new \TYPO3\Fluid\ViewHelpers\SectionViewHelper();
-		
+
 		$viewHelperNodeMock = $this->getMock('TYPO3\Fluid\Core\Parser\SyntaxTree\ViewHelperNode', array(), array(), '', FALSE);
 		$viewHelperArguments = array(
 			'name' => new \TYPO3\Fluid\Core\Parser\SyntaxTree\TextNode('sectionName')
 		);
-		
+
 		$variableContainer = new \TYPO3\Fluid\Core\ViewHelper\TemplateVariableContainer();
-		
+
 		$section->postParseEvent($viewHelperNodeMock, $viewHelperArguments, $variableContainer);
-		
+
 		$this->assertTrue($variableContainer->exists('sections'), 'Sections array was not created, albeit it should.');
 		$sections = $variableContainer->get('sections');
 		$this->assertEquals($sections['sectionName'], $viewHelperNodeMock, 'ViewHelperNode for section was not stored.');
