0f4d1d41e9e9eaf55ad04293cd6ec3c9e144b0a6
Karsten Dambekalns <karsten@typo3.org>
2013-03-28 10:50:44 +0100

    [TASK] Remove leading backslash in use statement(s)
    Change-Id: If99654189ac69742c95c1de8096a01e51471661c

--- Tests/Unit/ViewHelpers/Format/CaseViewHelperTest.php
+++ Tests/Unit/ViewHelpers/Format/CaseViewHelperTest.php
@@ -11,7 +11,7 @@ namespace TYPO3\Fluid\Tests\Unit\ViewHelpers\Format;
  * The TYPO3 project - inspiring people to share!                         *
  *                                                                        */
 
-use \TYPO3\Fluid\ViewHelpers\Format\CaseViewHelper;
+use TYPO3\Fluid\ViewHelpers\Format\CaseViewHelper;
 
 /**
  */
