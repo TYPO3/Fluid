594a8dd36a5ffae50e354769c253b03a67e02fb5
Bastian Waidelich <bastian@typo3.org>
2013-09-04 11:21:26 +0200

    [TASK] Adjust to "HTTP components for handling requests"
    This change replaces the ``AjaxWidgetRoutingAspect`` by a custom
    HTTP component that intercepts AJAX widget requests.
    
    Background:
    
    Fluids custom AJAX widget handling using AOP does not work together
    with the HTTP components change in Flow (#52064).
    Instead of adjusting the AOP aspect this change makes use of the new
    feature by adding the ``AjaxWidgetComponent``.
    
    Change-Id: Icf9895c86580f10ae92146037782c4693ec63ad5
    Depends: Iac1bd27cd1f2869e597b696c896633f14703ec40
    Related: #52064
    Releases: master

---
+++ Classes/TYPO3/Fluid/Core/Widget/AjaxWidgetComponent.php
@@ -0,0 +1,121 @@
+<?php
+namespace TYPO3\Fluid\Core\Widget;
+
+/*
+ * This script belongs to the TYPO3 Flow package "TYPO3.Fluid".           *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU Lesser General Public License, either version 3   *
+ * of the License, or (at your option) any later version.                 *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+use TYPO3\Flow\Annotations as Flow;
+use TYPO3\Flow\Http\Component\Exception as ComponentException;
+use TYPO3\Flow\Http\Request;
+use TYPO3\Flow\Http\Component\ComponentContext;
+use TYPO3\Flow\Http\Component\ComponentInterface;
+use TYPO3\Flow\Mvc\ActionRequest;
+use TYPO3\Flow\Mvc\Dispatcher;
+use TYPO3\Flow\Object\ObjectManagerInterface;
+use TYPO3\Flow\Security\Context;
+use TYPO3\Flow\Security\Cryptography\HashService;
+
+/**
+ * A HTTP component specifically for Ajax widgets
+ * It's task is to interrupt the default dispatching as soon as possible if the current request is an AJAX request
+ * triggered by a Fluid widget (e.g. contains the arguments "__widgetId" or "__widgetContext").
+ */
+class AjaxWidgetComponent implements ComponentInterface {
+
+	/**
+	 * @Flow\Inject
+	 * @var HashService
+	 */
+	protected $hashService;
+
+	/**
+	 * @Flow\Inject
+	 * @var AjaxWidgetContextHolder
+	 */
+	protected $ajaxWidgetContextHolder;
+
+	/**
+	 * @Flow\Inject
+	 * @var Dispatcher
+	 */
+	protected $dispatcher;
+
+	/**
+	 * @Flow\Inject
+	 * @var ObjectManagerInterface
+	 */
+	protected $objectManager;
+
+	/**
+	 * @Flow\Inject
+	 * @var Context
+	 */
+	protected $securityContext;
+
+	/**
+	 * @var array
+	 */
+	protected $options;
+
+	/**
+	 * @param array $options
+	 */
+	public function __construct(array $options = array()) {
+		$this->options = $options;
+	}
+
+	/**
+	 * Check if the current request contains a widget context.
+	 * If so dispatch it directly, otherwise continue with the next HTTP component.
+	 *
+	 * @param ComponentContext $componentContext
+	 * @return void
+	 * @throws ComponentException
+	 */
+	public function handle(ComponentContext $componentContext) {
+		$httpRequest = $componentContext->getHttpRequest();
+		$widgetContext = $this->extractWidgetContext($httpRequest);
+		if ($widgetContext === NULL) {
+			return;
+		}
+		/** @var $actionRequest ActionRequest */
+		$actionRequest = $this->objectManager->get('TYPO3\Flow\Mvc\ActionRequest', $httpRequest);
+		$actionRequest->setArgument('__widgetContext', $widgetContext);
+		$actionRequest->setControllerObjectName($widgetContext->getControllerObjectName());
+
+		$this->securityContext->setRequest($actionRequest);
+
+		$this->dispatcher->dispatch($actionRequest, $componentContext->getHttpResponse());
+
+		// stop processing the current component chain
+		$componentContext->setParameter('TYPO3\Flow\Http\Component\ComponentChain', 'cancel', TRUE);
+	}
+
+	/**
+	 * Extracts the WidgetContext from the given $httpRequest.
+	 * If the request contains an argument "__widgetId" the context is fetched from the session (AjaxWidgetContextHolder).
+	 * Otherwise the argument "__widgetContext" is expected to contain the serialized WidgetContext (protected by a HMAC suffix)
+	 *
+	 * @param Request $httpRequest
+	 * @return WidgetContext
+	 */
+	protected function extractWidgetContext(Request $httpRequest) {
+		if ($httpRequest->hasArgument('__widgetId')) {
+			return $this->ajaxWidgetContextHolder->get($httpRequest->getArgument('__widgetId'));
+		} elseif ($httpRequest->hasArgument('__widgetContext')) {
+			$serializedWidgetContextWithHmac = $httpRequest->getArgument('__widgetContext');
+			$serializedWidgetContext = $this->hashService->validateAndStripHmac($serializedWidgetContextWithHmac);
+			return unserialize(base64_decode($serializedWidgetContext));
+		}
+		return NULL;
+	}
+
+}
+?>
---
+++ Tests/Unit/Core/Widget/AjaxWidgetComponentTest.php
@@ -0,0 +1,216 @@
+<?php
+namespace TYPO3\Fluid\Tests\Unit\Core\Widget;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package "TYPO3.Fluid".           *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU Lesser General Public License, either version 3   *
+ * of the License, or (at your option) any later version.                 *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+use TYPO3\Flow\Http\Component\ComponentContext;
+use TYPO3\Flow\Http;
+use TYPO3\Flow\Mvc\Dispatcher;
+use TYPO3\Flow\Object\ObjectManagerInterface;
+use TYPO3\Flow\Security\Context;
+use TYPO3\Flow\Security\Cryptography\HashService;
+use TYPO3\Flow\Tests\UnitTestCase;
+use TYPO3\Fluid\Core\Widget\AjaxWidgetComponent;
+use TYPO3\Fluid\Core\Widget\AjaxWidgetContextHolder;
+
+/**
+ * Testcase for AjaxWidgetComponent
+ *
+ */
+class AjaxWidgetComponentTest extends UnitTestCase {
+
+	/**
+	 * @var AjaxWidgetComponent
+	 */
+	protected $ajaxWidgetComponent;
+
+	/**
+	 * @var ObjectManagerInterface|\PHPUnit_Framework_MockObject_MockObject
+	 */
+	protected $mockObjectManager;
+
+	/**
+	 * @var ComponentContext|\PHPUnit_Framework_MockObject_MockObject
+	 */
+	protected $mockComponentContext;
+
+	/**
+	 * @var Http\Request|\PHPUnit_Framework_MockObject_MockObject
+	 */
+	protected $mockHttpRequest;
+
+	/**
+	 * @var Http\Response|\PHPUnit_Framework_MockObject_MockObject
+	 */
+	protected $mockHttpResponse;
+
+	/**
+	 * @var AjaxWidgetContextHolder|\PHPUnit_Framework_MockObject_MockObject
+	 */
+	protected $mockAjaxWidgetContextHolder;
+
+	/**
+	 * @var HashService|\PHPUnit_Framework_MockObject_MockObject
+	 */
+	protected $mockHashService;
+
+	/**
+	 * @var Dispatcher|\PHPUnit_Framework_MockObject_MockObject
+	 */
+	protected $mockDispatcher;
+
+	/**
+	 * @var Context|\PHPUnit_Framework_MockObject_MockObject
+	 */
+	protected $mockSecurityContext;
+
+	/**
+	 */
+	public function setUp() {
+		$this->ajaxWidgetComponent = new AjaxWidgetComponent();
+
+		$this->mockObjectManager = $this->getMockBuilder('TYPO3\Flow\Object\ObjectManagerInterface')->getMock();
+		$this->inject($this->ajaxWidgetComponent, 'objectManager', $this->mockObjectManager);
+
+		$this->mockComponentContext = $this->getMockBuilder('TYPO3\Flow\Http\Component\ComponentContext')->disableOriginalConstructor()->getMock();
+
+		$this->mockHttpRequest = $this->getMockBuilder('TYPO3\Flow\Http\Request')->disableOriginalConstructor()->getMock();
+		$this->mockComponentContext->expects($this->any())->method('getHttpRequest')->will($this->returnValue($this->mockHttpRequest));
+
+		$this->mockHttpResponse = $this->getMockBuilder('TYPO3\Flow\Http\Response')->disableOriginalConstructor()->getMock();
+		$this->mockComponentContext->expects($this->any())->method('getHttpResponse')->will($this->returnValue($this->mockHttpResponse));
+
+		$this->mockAjaxWidgetContextHolder = $this->getMockBuilder('TYPO3\Fluid\Core\Widget\AjaxWidgetContextHolder')->getMock();
+		$this->inject($this->ajaxWidgetComponent, 'ajaxWidgetContextHolder', $this->mockAjaxWidgetContextHolder);
+
+		$this->mockHashService = $this->getMockBuilder('TYPO3\Flow\Security\Cryptography\HashService')->getMock();
+		$this->inject($this->ajaxWidgetComponent, 'hashService', $this->mockHashService);
+
+		$this->mockDispatcher = $this->getMockBuilder('TYPO3\Flow\Mvc\Dispatcher')->getMock();
+		$this->inject($this->ajaxWidgetComponent, 'dispatcher', $this->mockDispatcher);
+
+		$this->mockSecurityContext = $this->getMockBuilder('TYPO3\Flow\Security\Context')->getMock();
+		$this->inject($this->ajaxWidgetComponent, 'securityContext', $this->mockSecurityContext);
+
+
+	}
+
+	/**
+	 * @test
+	 */
+	public function handleDoesNotCreateActionRequestIfHttpRequestContainsNoWidgetContext() {
+		$this->mockHttpRequest->expects($this->at(0))->method('hasArgument')->with('__widgetId')->will($this->returnValue(FALSE));
+		$this->mockHttpRequest->expects($this->at(1))->method('hasArgument')->with('__widgetContext')->will($this->returnValue(FALSE));
+
+		$this->mockObjectManager->expects($this->never())->method('get');
+
+		$this->ajaxWidgetComponent->handle($this->mockComponentContext);
+	}
+
+	/**
+	 * @test
+	 */
+	public function handleSetsWidgetContextAndControllerObjectNameIfWidgetIdIsPresent() {
+		$mockWidgetId = 'SomeWidgetId';
+		$mockControllerObjectName = 'SomeControllerObjectName';
+		$this->mockHttpRequest->expects($this->at(0))->method('hasArgument')->with('__widgetId')->will($this->returnValue(TRUE));
+		$this->mockHttpRequest->expects($this->atLeastOnce())->method('getArgument')->with('__widgetId')->will($this->returnValue($mockWidgetId));
+		$mockWidgetContext = $this->getMockBuilder('TYPO3\Fluid\Core\Widget\WidgetContext')->getMock();
+		$mockWidgetContext->expects($this->atLeastOnce())->method('getControllerObjectName')->will($this->returnValue($mockControllerObjectName));
+		$this->mockAjaxWidgetContextHolder->expects($this->atLeastOnce())->method('get')->with($mockWidgetId)->will($this->returnValue($mockWidgetContext));
+		$mockActionRequest = $this->getMockBuilder('TYPO3\Flow\Mvc\ActionRequest')->disableOriginalConstructor()->getMock();
+		$this->mockObjectManager->expects($this->atLeastOnce())->method('get')->with('TYPO3\Flow\Mvc\ActionRequest')->will($this->returnValue($mockActionRequest));
+
+		$mockActionRequest->expects($this->once())->method('setArgument')->with('__widgetContext', $mockWidgetContext);
+		$mockActionRequest->expects($this->once())->method('setControllerObjectName')->with($mockControllerObjectName);
+
+		$this->ajaxWidgetComponent->handle($this->mockComponentContext);
+	}
+
+	/**
+	 * @test
+	 */
+	public function handleDispatchesActionRequestIfWidgetContextIsPresent() {
+		$mockWidgetId = 'SomeWidgetId';
+		$mockControllerObjectName = 'SomeControllerObjectName';
+		$this->mockHttpRequest->expects($this->at(0))->method('hasArgument')->with('__widgetId')->will($this->returnValue(TRUE));
+		$this->mockHttpRequest->expects($this->atLeastOnce())->method('getArgument')->with('__widgetId')->will($this->returnValue($mockWidgetId));
+		$mockWidgetContext = $this->getMockBuilder('TYPO3\Fluid\Core\Widget\WidgetContext')->getMock();
+		$mockWidgetContext->expects($this->atLeastOnce())->method('getControllerObjectName')->will($this->returnValue($mockControllerObjectName));
+		$this->mockAjaxWidgetContextHolder->expects($this->atLeastOnce())->method('get')->with($mockWidgetId)->will($this->returnValue($mockWidgetContext));
+		$mockActionRequest = $this->getMockBuilder('TYPO3\Flow\Mvc\ActionRequest')->disableOriginalConstructor()->getMock();
+		$this->mockObjectManager->expects($this->atLeastOnce())->method('get')->with('TYPO3\Flow\Mvc\ActionRequest')->will($this->returnValue($mockActionRequest));
+
+		$this->mockDispatcher->expects($this->once())->method('dispatch')->with($mockActionRequest, $this->mockHttpResponse);
+
+		$this->ajaxWidgetComponent->handle($this->mockComponentContext);
+	}
+
+	/**
+	 * @test
+	 */
+	public function handleCancelsComponentChainIfWidgetContextIsPresent() {
+		$mockWidgetId = 'SomeWidgetId';
+		$mockControllerObjectName = 'SomeControllerObjectName';
+		$this->mockHttpRequest->expects($this->at(0))->method('hasArgument')->with('__widgetId')->will($this->returnValue(TRUE));
+		$this->mockHttpRequest->expects($this->atLeastOnce())->method('getArgument')->with('__widgetId')->will($this->returnValue($mockWidgetId));
+		$mockWidgetContext = $this->getMockBuilder('TYPO3\Fluid\Core\Widget\WidgetContext')->getMock();
+		$mockWidgetContext->expects($this->atLeastOnce())->method('getControllerObjectName')->will($this->returnValue($mockControllerObjectName));
+		$this->mockAjaxWidgetContextHolder->expects($this->atLeastOnce())->method('get')->with($mockWidgetId)->will($this->returnValue($mockWidgetContext));
+		$mockActionRequest = $this->getMockBuilder('TYPO3\Flow\Mvc\ActionRequest')->disableOriginalConstructor()->getMock();
+		$this->mockObjectManager->expects($this->atLeastOnce())->method('get')->with('TYPO3\Flow\Mvc\ActionRequest')->will($this->returnValue($mockActionRequest));
+
+		$this->mockComponentContext->expects($this->once())->method('setParameter')->with('TYPO3\Flow\Http\Component\ComponentChain', 'cancel', TRUE);
+
+		$this->ajaxWidgetComponent->handle($this->mockComponentContext);
+	}
+
+	/**
+	 * @test
+	 */
+	public function handleInjectsActionRequestToSecurityContext() {
+		$mockWidgetId = 'SomeWidgetId';
+		$mockControllerObjectName = 'SomeControllerObjectName';
+		$this->mockHttpRequest->expects($this->at(0))->method('hasArgument')->with('__widgetId')->will($this->returnValue(TRUE));
+		$this->mockHttpRequest->expects($this->atLeastOnce())->method('getArgument')->with('__widgetId')->will($this->returnValue($mockWidgetId));
+		$mockWidgetContext = $this->getMockBuilder('TYPO3\Fluid\Core\Widget\WidgetContext')->getMock();
+		$mockWidgetContext->expects($this->atLeastOnce())->method('getControllerObjectName')->will($this->returnValue($mockControllerObjectName));
+		$this->mockAjaxWidgetContextHolder->expects($this->atLeastOnce())->method('get')->with($mockWidgetId)->will($this->returnValue($mockWidgetContext));
+		$mockActionRequest = $this->getMockBuilder('TYPO3\Flow\Mvc\ActionRequest')->disableOriginalConstructor()->getMock();
+		$this->mockObjectManager->expects($this->atLeastOnce())->method('get')->with('TYPO3\Flow\Mvc\ActionRequest')->will($this->returnValue($mockActionRequest));
+
+
+		$this->mockSecurityContext->expects($this->once())->method('setRequest')->with($mockActionRequest);
+
+		$this->ajaxWidgetComponent->handle($this->mockComponentContext);
+	}
+
+	/**
+	 * @test
+	 */
+	public function extractWidgetContextDecodesSerializedWidgetContextIfPresent() {
+		$ajaxWidgetComponent = $this->getAccessibleMock('TYPO3\Fluid\Core\Widget\AjaxWidgetComponent', array('dummy'));
+		$this->inject($ajaxWidgetComponent, 'hashService', $this->mockHashService);
+
+		$mockWidgetContext = 'SomeWidgetContext';
+		$mockSerializedWidgetContext = base64_encode(serialize($mockWidgetContext));
+		$mockSerializedWidgetContextWithHmac = $mockSerializedWidgetContext . 'HMAC';
+		$this->mockHttpRequest->expects($this->at(0))->method('hasArgument')->with('__widgetId')->will($this->returnValue(FALSE));
+		$this->mockHttpRequest->expects($this->at(1))->method('hasArgument')->with('__widgetContext')->will($this->returnValue(TRUE));
+		$this->mockHttpRequest->expects($this->atLeastOnce())->method('getArgument')->with('__widgetContext')->will($this->returnValue($mockSerializedWidgetContextWithHmac));
+		$this->mockHashService->expects($this->atLeastOnce())->method('validateAndStripHmac')->with($mockSerializedWidgetContextWithHmac)->will($this->returnValue($mockSerializedWidgetContext));
+
+		$actualResult = $ajaxWidgetComponent->_call('extractWidgetContext', $this->mockHttpRequest);
+		$this->assertEquals($mockWidgetContext, $actualResult);
+	}
+
+}
+?>
