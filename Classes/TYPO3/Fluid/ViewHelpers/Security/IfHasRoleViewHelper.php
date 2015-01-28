6f3f9030aea25b4ec88929cc37b4c93762ddf9a7
Robert Lemke <robert@typo3.org>
2015-01-28 21:59:54 +0100

    [FEATURE] "account" option for security.ifHasRole view helper
    This change introduces a new option "account" for the ifHasRole view
    helper which allows for specifying an account other than the currently
    authenticated account.
    
    Additionally the "role" option now allows roles to be specified as
    Role objects.
    
    Change-Id: I034aec420bc5400cd7ade8f1c4a9f0d8d8abc64f
    Releases: master
    Resolves: FLOW-182

--- Classes/TYPO3/Fluid/ViewHelpers/Security/IfHasRoleViewHelper.php
+++ Classes/TYPO3/Fluid/ViewHelpers/Security/IfHasRoleViewHelper.php
@@ -13,7 +13,10 @@ namespace TYPO3\Fluid\ViewHelpers\Security;
 
 
 use TYPO3\Flow\Annotations as Flow;
+use TYPO3\Flow\Security\Account;
 use TYPO3\Flow\Security\Context;
+use TYPO3\Flow\Security\Policy\PolicyService;
+use TYPO3\Flow\Security\Policy\Role;
 use TYPO3\Fluid\Core\ViewHelper\AbstractConditionViewHelper;
 
 /**
@@ -56,6 +59,19 @@ use TYPO3\Fluid\Core\ViewHelper\AbstractConditionViewHelper;
  * Everything inside the "then" tag is displayed if you have the role.
  * Otherwise, everything inside the "else"-tag is displayed.
  *
+ * <code title="Usage with role object in role attribute">
+ * <f:security.ifHasRole role="{someRoleObject}">
+ *   This is being shown in case you have the specified role
+ * </f:security.ifHasRole>
+ * </code>
+ *
+ * <code title="Usage with specific account instead of currently logged in account">
+ * <f:security.ifHasRole role="Administrator" account="{otherAccount}">
+ *   This is being shown in case "otherAccount" has the Acme.MyPackage:Administrator role (aka role).
+ * </f:security.ifHasRole>
+ * </code>
+ *
+ *
  * @api
  */
 class IfHasRoleViewHelper extends AbstractConditionViewHelper {
@@ -67,25 +83,48 @@ class IfHasRoleViewHelper extends AbstractConditionViewHelper {
 	protected $securityContext;
 
 	/**
+	 * @Flow\Inject
+	 * @var PolicyService
+	 */
+	protected $policyService;
+
+	/**
 	 * renders <f:then> child if the role could be found in the security context,
 	 * otherwise renders <f:else> child.
 	 *
-	 * @param string $role The role
+	 * @param string $role The role or role identifier
 	 * @param string $packageKey PackageKey of the package defining the role
+	 * @param Account $account If specified, this subject of this check is the given Account instead of the currently authenticated account
 	 * @return string the rendered string
 	 * @api
 	 */
-	public function render($role, $packageKey = NULL) {
-		if ($role !== 'Everybody' && $role !== 'Anonymous' && $role !== 'AuthenticatedUser' && strpos($role, '.') === FALSE && strpos($role, ':') === FALSE) {
-			if ($packageKey === NULL) {
-				$request = $this->controllerContext->getRequest();
-				$role = $request->getControllerPackageKey() . ':' . $role;
-			} else {
-				$role = $packageKey . ':' . $role;
+	public function render($role, $packageKey = NULL, Account $account = NULL) {
+		if (is_string($role)) {
+			$roleIdentifier = $role;
+
+			if (in_array($roleIdentifier, array('Everybody', 'Anonymous', 'AuthenticatedUser'))) {
+				 $roleIdentifier = 'TYPO3.Flow:' . $roleIdentifier;
 			}
+
+			if (strpos($roleIdentifier, '.') === FALSE && strpos($roleIdentifier, ':') === FALSE) {
+				if ($packageKey === NULL) {
+					$request = $this->controllerContext->getRequest();
+					$roleIdentifier = $request->getControllerPackageKey() . ':' . $roleIdentifier;
+				} else {
+					$roleIdentifier = $packageKey . ':' . $roleIdentifier;
+				}
+			}
+
+			$role = $this->policyService->getRole($roleIdentifier);
 		}
 
-		if ($this->securityContext->hasRole($role)) {
+		if ($account instanceof Account) {
+			$hasRole = $account->hasRole($role);
+		} else {
+			$hasRole = $this->securityContext->hasRole($role->getIdentifier());
+		}
+
+		if ($hasRole) {
 			return $this->renderThenChild();
 		} else {
 			return $this->renderElseChild();
--- Tests/Unit/ViewHelpers/Security/IfHasRoleViewHelperTest.php
+++ Tests/Unit/ViewHelpers/Security/IfHasRoleViewHelperTest.php
@@ -11,18 +11,21 @@ namespace TYPO3\Fluid\Tests\Unit\ViewHelpers\Security;
  * The TYPO3 project - inspiring people to share!                         *
  *                                                                        */
 
+use TYPO3\Flow\Http\Request;
+use TYPO3\Flow\Http\Uri;
 use TYPO3\Flow\Reflection\ObjectAccess;
 use TYPO3\Flow\Security\Policy\PolicyService;
 use TYPO3\Flow\Security\Policy\Role;
 use TYPO3\Fluid\ViewHelpers\Security\IfHasRoleViewHelper;
+use TYPO3\Fluid\ViewHelpers\ViewHelperBaseTestcase;
 
 require_once(__DIR__ . '/../ViewHelperBaseTestcase.php');
 
 /**
- * Testcase for IfHasRoleViewHelper
+ * Test case for IfHasRoleViewHelper
  *
  */
-class IfHasRoleViewHelperTest extends \TYPO3\Fluid\ViewHelpers\ViewHelperBaseTestcase {
+class IfHasRoleViewHelperTest extends ViewHelperBaseTestcase {
 
 	/**
 	 * Create a mock controllerContext
@@ -30,9 +33,9 @@ class IfHasRoleViewHelperTest extends \TYPO3\Fluid\ViewHelpers\ViewHelperBaseTes
 	 * @return \PHPUnit_Framework_MockObject_MockObject
 	 */
 	protected function getMockControllerContext() {
-		$httpRequest = \TYPO3\Flow\Http\Request::create(new \TYPO3\Flow\Http\Uri('http://robertlemke.com/blog'));
+		$httpRequest = Request::create(new Uri('http://robertlemke.com/blog'));
 		$mockRequest = $this->getMock('TYPO3\Flow\Mvc\ActionRequest', array(), array($httpRequest));
-		$mockRequest->expects($this->any())->method('getControllerPackageKey')->will($this->returnValue('TYPO3.Fluid'));
+		$mockRequest->expects($this->any())->method('getControllerPackageKey')->will($this->returnValue('Acme.Demo'));
 
 		$mockControllerContext = $this->getMock('TYPO3\Flow\Mvc\Controller\ControllerContext', array('getRequest'), array(), '', FALSE);
 		$mockControllerContext->expects($this->any())->method('getRequest')->will($this->returnValue($mockRequest));
@@ -44,16 +47,23 @@ class IfHasRoleViewHelperTest extends \TYPO3\Fluid\ViewHelpers\ViewHelperBaseTes
 	 * @test
 	 */
 	public function viewHelperRendersThenPartIfHasRoleReturnsTrue() {
+		$role = new Role('Acme.Demo:SomeRole');
+
 		$mockSecurityContext = $this->getMock('TYPO3\Flow\Security\Context', array(), array(), '', FALSE);
-		$mockSecurityContext->expects($this->once())->method('hasRole')->with('TYPO3.Fluid:someGA')->will($this->returnValue(TRUE));
+		$mockSecurityContext->expects($this->once())->method('hasRole')->with('Acme.Demo:SomeRole')->will($this->returnValue(TRUE));
+
+		$mockPolicyService = $this->getMock('TYPO3\Flow\Security\Policy\PolicyService', array(), array(), '', FALSE);
+		$mockPolicyService->expects($this->once())->method('getRole')->with('Acme.Demo:SomeRole')->will($this->returnValue($role));
 
 		$mockViewHelper = $this->getMock('TYPO3\Fluid\ViewHelpers\Security\IfHasRoleViewHelper', array('renderThenChild', 'hasAccessToPrivilege'));
 		$this->inject($mockViewHelper, 'securityContext', $mockSecurityContext);
 		$this->inject($mockViewHelper, 'controllerContext', $this->getMockControllerContext());
-		$mockViewHelper->expects($this->once())->method('renderThenChild')->will($this->returnValue('foo'));
+		$this->inject($mockViewHelper, 'policyService', $mockPolicyService);
+		$mockViewHelper->expects($this->once())->method('renderThenChild')->will($this->returnValue('then-child'));
 
-		$actualResult = $mockViewHelper->render('someGA');
-		$this->assertEquals('foo', $actualResult);
+		/** @var IfHasRoleViewHelper $mockViewHelper */
+		$actualResult = $mockViewHelper->render('SomeRole');
+		$this->assertEquals('then-child', $actualResult);
 	}
 
 	/**
@@ -67,11 +77,8 @@ class IfHasRoleViewHelperTest extends \TYPO3\Fluid\ViewHelpers\ViewHelperBaseTes
 					return TRUE;
 				case 'TYPO3.Fluid:User':
 					return FALSE;
-				case 'Everybody':
-					return TRUE;
-				default:
-					return FALSE;
 			}
+
 		}));
 
 		$mockViewHelper = $this->getMock('TYPO3\Fluid\ViewHelpers\Security\IfHasRoleViewHelper', array('renderThenChild', 'renderElseChild', 'hasAccessToPrivilege'));
@@ -80,35 +87,32 @@ class IfHasRoleViewHelperTest extends \TYPO3\Fluid\ViewHelpers\ViewHelperBaseTes
 		$mockViewHelper->expects($this->any())->method('renderThenChild')->will($this->returnValue('true'));
 		$mockViewHelper->expects($this->any())->method('renderElseChild')->will($this->returnValue('false'));
 
-		$actualResult = $mockViewHelper->render('TYPO3.Fluid:Administrator');
+		$actualResult = $mockViewHelper->render(new Role('TYPO3.Fluid:Administrator'));
 		$this->assertEquals('true', $actualResult, 'Full role identifier in role argument is accepted');
 
-		$actualResult = $mockViewHelper->render('Administrator');
-		$this->assertEquals('true', $actualResult, 'Packagekey from controllerContext is automatically prepended if packageKey is absent');
-
-		$actualResult = $mockViewHelper->render('Everybody');
-		$this->assertEquals('true', $actualResult);
-
-		$actualResult = $mockViewHelper->render('Administrator', 'TYPO3.Fluid');
-		$this->assertEquals('true', $actualResult);
-
-		$actualResult = $mockViewHelper->render('User', 'TYPO3.Fluid');
+		$actualResult = $mockViewHelper->render(new Role('TYPO3.Fluid:User'), 'TYPO3.Fluid');
 		$this->assertEquals('false', $actualResult);
 	}
 
 	/**
 	 * @test
 	 */
-	public function viewHelperRendersElsePartIfConditionIsFalse() {
-		$mockSecurityContext = $this->getMock('TYPO3\Flow\Security\Context', array(), array(), '', FALSE);
-		$mockSecurityContext->expects($this->once())->method('hasRole')->with('TYPO3.Fluid:someGA')->will($this->returnValue(FALSE));
+	public function viewHelperUsesSpecifiedAccountForCheck() {
+		$mockAccount = $this->getMock('TYPO3\Flow\Security\Account');
+		$mockAccount->expects($this->any())->method('hasRole')->will($this->returnCallback(function(Role $role) {
+			switch($role->getIdentifier()) {
+				case 'TYPO3.Fluid:Administrator':
+					return TRUE;
+			}
+		}));
 
-		$mockViewHelper = $this->getMock('TYPO3\Fluid\ViewHelpers\Security\IfHasRoleViewHelper', array('renderElseChild'));
-		$mockViewHelper->expects($this->once())->method('renderElseChild')->will($this->returnValue('ElseViewHelperResults'));
-		$this->inject($mockViewHelper, 'securityContext', $mockSecurityContext);
+		$mockViewHelper = $this->getMock('TYPO3\Fluid\ViewHelpers\Security\IfHasRoleViewHelper', array('renderThenChild', 'renderElseChild', 'hasAccessToPrivilege'));
 		$this->inject($mockViewHelper, 'controllerContext', $this->getMockControllerContext());
+		$mockViewHelper->expects($this->any())->method('renderThenChild')->will($this->returnValue('true'));
+		$mockViewHelper->expects($this->any())->method('renderElseChild')->will($this->returnValue('false'));
 
-		$actualResult = $mockViewHelper->render('someGA');
-		$this->assertEquals('ElseViewHelperResults', $actualResult);
+		/** @var IfHasRoleViewHelper $mockViewHelper */
+		$actualResult = $mockViewHelper->render(new Role('TYPO3.Fluid:Administrator'), NULL, $mockAccount);
+		$this->assertEquals('true', $actualResult, 'Full role identifier in role argument is accepted');
 	}
 }
