a3aa204524d3ddfef8ef956f8df19844f06e4712
Claus Due <claus@namelesscoder.net>
2016-10-02 18:48:43 +0200

    [BUGFIX] Fix CLI usage of class names (#178)
    Correct invalid references to class names and refactor the main command entry method to correct class construction.

---
+++ tests/Functional/CommandTest.php
@@ -0,0 +1,54 @@
+<?php
+namespace FluidTYPO3Fluid\Flux\Tests\Unit\Functional;
+
+/*
+ * This file belongs to the package "TYPO3 Fluid".
+ * See LICENSE.txt that was shipped with this package.
+ */
+
+use org\bovigo\vfs\vfsStream;
+use TYPO3Fluid\Fluid\Tests\BaseTestCase;
+
+/**
+ * Class CommandTest
+ */
+class CommandTest extends BaseTestCase {
+
+	/**
+	 * @return void
+	 */
+	public static function setUpBeforeClass() {
+		vfsStream::setup('fakecache/');
+	}
+
+	/**
+	 * @param string $argumentString
+	 * @param array $mustContain
+	 * @param array $mustNotContain
+	 * @dataProvider getCommandTestValues
+	 */
+	public function testCommand($argumentString, array $mustContain, array $mustNotContain) {
+		$bin = realpath(__DIR__ . '/../../bin/fluid');
+		$command = sprintf($argumentString, $bin);
+		$output = shell_exec($command);
+		foreach ($mustContain as $mustContainString) {
+			$this->assertContains($mustContainString, $output);
+		}
+		foreach ($mustNotContain as $mustNotContainString) {
+			$this->assertNotContains($mustNotContainString, $output);
+		}
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getCommandTestValues() {
+		$dummyVariablesFile = realpath(__DIR__ . '/Fixtures/Variables/DummyVariables.json');
+		return array(
+			array('%s --help', array('Use the CLI utility in the following modes'), array('Exception')),
+			array('echo "Hello world!" | %s', array('Hello world!'), array('Exeption')),
+			array('echo "{foo}" | %s --variables "{\\"foo\\": \\"bar\\"}"', array('bar'), array('Exception', 'foo')),
+		);
+	}
+
+}
