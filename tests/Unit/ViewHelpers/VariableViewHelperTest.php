f4e8b2dc3fea6c62f61f1462a31fd2c2d938e42c
Claus Due <claus@namelesscoder.net>
2017-01-10 20:51:03 +0100

    [FEATURE] Add variable assigning ViewHelper (#248)
    New ViewHelper `f:variable` with arguments "name" and "value", assigns a variable in the VariableProvider using the `add()` method and does not remove the variable again afterwards.
    
    Usages:
    
        {f:variable(name: 'myvariable', value: 'some value')}
        <f:variable name="myvariable">some value</f:variable>
        {oldvariable -> f:format.htmlspecialchars() -> f:variable(name: 'newvariable')}
        <f:variable name="myvariable"><f:format.htmlspecialchars>{oldvariable}</f:format.htmlspecialchars></f:variable>

---
+++ tests/Unit/ViewHelpers/VariableViewHelperTest.php
@@ -0,0 +1,31 @@
+<?php
+namespace TYPO3Fluid\Fluid\Tests\Unit\ViewHelpers;
+
+/*
+ * This file belongs to the package "TYPO3 Fluid".
+ * See LICENSE.txt that was shipped with this package.
+ */
+
+use TYPO3Fluid\Fluid\Core\Variables\StandardVariableProvider;
+use TYPO3Fluid\Fluid\Tests\Unit\Core\Rendering\RenderingContextFixture;
+use TYPO3Fluid\Fluid\ViewHelpers\VariableViewHelper;
+
+/**
+ * Testcase for VariableViewHelper
+ */
+class VariableViewHelperTest extends ViewHelperBaseTestcase
+{
+
+    /**
+     * @test
+     */
+    public function assignsVariableInVariableProvider()
+    {
+        $variableProvider = $this->getMockBuilder(StandardVariableProvider::class)->setMethods(['add'])->getMock();
+        $variableProvider->expects($this->once())->method('add')->with('name', 'value');
+        $renderingContext = new RenderingContextFixture();
+        $renderingContext->setVariableProvider($variableProvider);
+        VariableViewHelper::renderStatic(['name' => 'name', 'value' => null], function() { return 'value'; }, $renderingContext);
+    }
+
+}
