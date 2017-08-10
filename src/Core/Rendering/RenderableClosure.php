70046ab6e7ba517e1cccb5d08e3c193b7dfe2e3d
Claus Due <claus@namelesscoder.net>
2017-08-10 23:59:45 +0200

    [FEATURE] Introduce concept of Renderables (#325)
    This patch adds a new interface for "renderable" objects, which allows any object to adopt the interface and when assigned to the template, render itself based on the current RenderingContext. A new argument "renderable" is added to `f:render` to render such objects. Two implementations are added: a StandardRenderable which renders a Node, and a ClosureRenderable which renders arbitrary closures.
    
    The feature is also the basis for a larger refactoring which finally stop storing sections in the VariableProvider while parsing, by providing a proper API for getting sections from a parsed template object.

---
+++ src/Core/Rendering/RenderableClosure.php
@@ -0,0 +1,40 @@
+<?php
+namespace TYPO3Fluid\Fluid\Core\Rendering;
+
+/*
+ * This file belongs to the package "TYPO3 Fluid".
+ * See LICENSE.txt that was shipped with this package.
+ */
+
+use TYPO3Fluid\Fluid\Core\Parser\SyntaxTree\NodeInterface;
+use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
+
+/**
+ * Class RenderableClosure
+ */
+class RenderableClosure extends AbstractRenderable
+{
+    /**
+     * @var \Closure
+     */
+    protected $closure;
+
+    /**
+     * @param \Closure $closure
+     * @return RenderableClosure
+     */
+    public function setClosure(\Closure $closure)
+    {
+        $this->closure = $closure;
+        return $this;
+    }
+
+    /**
+     * @param RenderingContextInterface $renderingContext
+     * @return mixed
+     */
+    public function render(RenderingContextInterface $renderingContext)
+    {
+        return call_user_func_array($this->closure, [$renderingContext, $this->node]);
+    }
+}
