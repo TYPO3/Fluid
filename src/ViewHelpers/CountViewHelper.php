0b90b562266eee549e4ae788f32f7bdb8bada013
Benni <benni@typo3.org>
2017-08-25 11:40:31 +0200

    Fixed missing import statement

--- src/ViewHelpers/CountViewHelper.php
+++ src/ViewHelpers/CountViewHelper.php
@@ -7,6 +7,7 @@ namespace TYPO3Fluid\Fluid\ViewHelpers;
  */
 
 use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
+use TYPO3Fluid\Fluid\Core\ViewHelper;
 use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
 use TYPO3Fluid\Fluid\Core\ViewHelper\Traits\CompileWithContentArgumentAndRenderStatic;
 
