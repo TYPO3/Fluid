34559183f935e0869379598c91789c2d40f1e65b
Oliver Eglseder <php@vxvr.de>
2018-02-05 13:10:50 +0100

    [BUGFIX] Use more reliable function parse_url to determine path schemes

--- src/View/TemplatePaths.php
+++ src/View/TemplatePaths.php
@@ -404,7 +404,7 @@ class TemplatePaths
         if (is_array($path)) {
             $paths = array_map([$this, 'sanitizePath'], $path);
             return array_unique($paths);
-        } elseif (($wrapper = substr($path, 0, strpos($path, ':'))) && in_array($wrapper, stream_get_wrappers())) {
+        } elseif (($wrapper = parse_url($path, PHP_URL_SCHEME)) && in_array($wrapper, stream_get_wrappers())) {
             return $path;
         } elseif (!empty($path)) {
             $path = str_replace(['\\', '//'], '/', (string) $path);
