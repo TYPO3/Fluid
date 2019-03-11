e9109ada988e8b67edd554da2a53b401bcf0ae04
Daniel Siepmann <coding@daniel-siepmann.de>
2019-03-11 13:59:01 +0100

    [TASK] Migrate documentation within phpdoc to rst
    This allows the TYPO3 documentation to render the ViewHelper Reference.

--- src/ViewHelpers/AliasViewHelper.php
+++ src/ViewHelpers/AliasViewHelper.php
@@ -14,30 +14,42 @@ use TYPO3Fluid\Fluid\Core\ViewHelper\Traits\CompileWithRenderStatic;
  * Declares new variables which are aliases of other variables.
  * Takes a "map"-Parameter which is an associative array which defines the shorthand mapping.
  *
- * The variables are only declared inside the <f:alias>...</f:alias>-tag. After the
+ * The variables are only declared inside the :html:`<f:alias>...</f:alias>` tag. After the
  * closing tag, all declared variables are removed again.
  *
- * = Examples =
+ * Examples
+ * ========
  *
- * <code title="Single alias">
- * <f:alias map="{x: 'foo'}">{x}</f:alias>
- * </code>
- * <output>
- * foo
- * </output>
+ * Single alias
+ * ------------
  *
- * <code title="Multiple mappings">
- * <f:alias map="{x: foo.bar.baz, y: foo.bar.baz.name}">
- *   {x.name} or {y}
- * </f:alias>
- * </code>
- * <output>
- * [name] or [name]
- * depending on {foo.bar.baz}
- * </output>
+ * ::
  *
- * Note: Using this view helper can be a sign of weak architecture. If you end up using it extensively
- * you might want to fine-tune your "view model" (the data you assign to the view).
+ *     <f:alias map="{x: 'foo'}">{x}</f:alias>
+ *
+ * Output::
+ *
+ *     foo
+ *
+ * Multiple mappings
+ * -----------------
+ *
+ * ::
+ *
+ *     <f:alias map="{x: foo.bar.baz, y: foo.bar.baz.name}">
+ *         {x.name} or {y}
+ *     </f:alias>
+ *
+ * Output::
+ *
+ *     [name] or [name]
+ *
+ * Depending on ``{foo.bar.baz}``.
+ *
+ * .. note::
+ *    Using this ViewHelper can be a sign of weak architecture. If you end up
+ *    using it extensively you might want to fine-tune your "view model" (the
+ *    data you assign to the view).
  *
  * @api
  */
--- src/ViewHelpers/Cache/WarmupViewHelper.php
+++ src/ViewHelpers/Cache/WarmupViewHelper.php
@@ -29,30 +29,38 @@ use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
  * template chunk can then be compiled using those default
  * variables.
  *
- * Note: this does not imply that only those variable values
- * will be used by the compiled template. It only means that
- * DEFAULT values of vital variables will be present during
- * compiling.
+ * .. note::
+ *    This does not imply that only those variable values will
+ *    be used by the compiled template. It only means that
+ *    DEFAULT values of vital variables will be present during
+ *    compiling.
  *
  * If you find yourself completely unable to properly warm up
  * a specific template file even with use of this ViewHelper,
- * then you can consider using `f:cache.disable` to prevent
- * the template compiler from even attempting to compile it.
+ * then you can consider using
+ * :ref:`f:cache.disable <typo3fluid-fluid-cache-disable>`
+ * to prevent the template compiler from even attempting to
+ * compile it.
  *
- * USE WITH CARE! SOME EDGE CASES OF FOR EXAMPLE VIEWHELPERS
- * WHICH REQUIRE SPECIAL VARIABLE TYPES MAY NOT BE SUPPORTED
- * HERE DUE TO THE RUDIMENTARY NATURE OF VARIABLES YOU DEFINE.
+ * .. caution::
+ *    USE WITH CARE! SOME EDGE CASES OF FOR EXAMPLE VIEWHELPERS
+ *    WHICH REQUIRE SPECIAL VARIABLE TYPES MAY NOT BE SUPPORTED
+ *    HERE DUE TO THE RUDIMENTARY NATURE OF VARIABLES YOU DEFINE.
  *
- * = Examples =
+ * Examples
+ * ========
  *
- * <code title="Usage and effect">
- * <f:cache.warmup variables="{foo: bar}">
- *    Template code depending on {foo} variable which is not
- *    assigned when warming up Fluid's caches. {foo} is only
- *    assigned if the variable does not already exist and the
- *    assignment only happens if Fluid is in warmup mode.
- * </f:cache.warmup>
- * </code>
+ * Usage and effect
+ * ----------------
+ *
+ * ::
+ *
+ *     <f:cache.warmup variables="{foo: bar}">
+ *        Template code depending on {foo} variable which is not
+ *        assigned when warming up Fluid's caches. {foo} is only
+ *        assigned if the variable does not already exist and the
+ *        assignment only happens if Fluid is in warmup mode.
+ *     </f:cache.warmup>
  *
  * @api
  */
