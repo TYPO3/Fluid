91d8b887817597ea2e46c1a10b073a2c1573a80a
Felix Althaus <cumuru@users.noreply.github.com>
2019-03-19 16:10:49 +0100

    [BUGFIX] Support overloaded methods when extracting variables (#438)
    Value extraction will now support delegation of getter calls
    via PHP‘s __call() magic method. Additionally, extracting a property
    that results in a protected getter call will stop raising an error.

--- src/Core/Variables/VariableExtractor.php
+++ src/Core/Variables/VariableExtractor.php
@@ -209,7 +209,7 @@ class VariableExtractor
         if (is_object($subject)) {
             $upperCasePropertyName = ucfirst($propertyName);
             $getter = 'get' . $upperCasePropertyName;
-            if (method_exists($subject, $getter)) {
+            if (is_callable([$subject, $getter])) {
                 return self::ACCESSOR_GETTER;
             }
             if ($this->isExtractableThroughAsserter($subject, $propertyName)) {
