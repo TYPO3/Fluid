f6375aab4301c2f4ab8125683f24670d575f0379
Claus Due <claus@namelesscoder.net>
2019-09-28 11:50:53 +0200

    [TASK] Remove old debug line

--- examples/include/autoload.php
+++ examples/include/autoload.php
@@ -2,7 +2,6 @@
 
 if (!class_exists(TYPO3Fluid\Fluid\View\TemplateView::class)) {
     foreach ([__DIR__ . '/../../vendor/autoload.php', __DIR__ . '/../../../../autoload.php'] as $possibleAutoloadLocation) {
-        #var_dump($possibleAutoloadLocation);
         if (file_exists($possibleAutoloadLocation)) {
             require_once $possibleAutoloadLocation;
         }
