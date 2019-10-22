fc2b02ae57bf9861045a4be569eb7075ffd3449c
Oliver Hader <oliver@typo3.org>
2019-10-22 22:59:02 +0200

    [TASK] Add tests in order to ensure accessor orders
    Related: #485

---
+++ tests/Functional/Cases/Rendering/Fixtures/MagicGetTrait.php
@@ -0,0 +1,19 @@
+<?php
+namespace TYPO3Fluid\Fluid\Tests\Functional\Cases\Rendering\Fixtures;
+
+trait MagicGetTrait
+{
+    use PropertiesTrait;
+
+    /**
+     * @param string $propertyName
+     * @return mixed|null
+     */
+    public function __get(string $propertyName)
+    {
+        if (property_exists($this, $propertyName)) {
+            return $this->{$propertyName} . sprintf('@__get(%s)', $propertyName);
+        }
+        return null;
+    }
+}
