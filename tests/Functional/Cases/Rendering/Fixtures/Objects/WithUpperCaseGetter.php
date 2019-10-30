c54bbee7c76d241998158836718e7d23d72b6655
Oliver Hader <oliver.hader@typo3.org>
2019-10-30 09:24:59 +0100

    [TASK] Add tests in order to ensure accessor orders (#486)
    * [TASK] Add tests in order to ensure accessor orders
    
    Related: #485
    
    * [TASK] Avoid using anonymous classes
    
    * [TASK] Support non existing properties in accessor tests
    
    * [TASK] Add non-existing property to __get/__call accessor tests
    
    * [BUGFIX] Adjust object accessor behavior
    
    * ensures basically behavior of v2.6.1
    * allows using `__call()` for existing private/protected properties
    * allows using `__call()` for non-existing properties (see #467)
    
    * [TASK] Remove tests for reverted behaviors
    
    Brings test expectations to match 2.6.1 behavior
    without support for overloaded methods. Removes
    tests and fixtures which dealt with overloads.
    
    * [TASK] Remove unused internal test methods
    
    * [TASK] Remove unrelated change to composer.json
    
    Will be introduced in separate patch.
    
    * [TASK] Add missing EOF line feeds

---
+++ tests/Functional/Cases/Rendering/Fixtures/Objects/WithUpperCaseGetter.php
@@ -0,0 +1,10 @@
+<?php
+namespace TYPO3Fluid\Fluid\Tests\Functional\Cases\Rendering\Fixtures\Objects;
+
+use TYPO3Fluid\Fluid\Tests\Functional\Cases\Rendering\Fixtures\Traits;
+
+class WithUpperCaseGetter
+{
+    use Traits\PropertiesTrait;
+    use Traits\UpperCaseGetterTrait;
+}
