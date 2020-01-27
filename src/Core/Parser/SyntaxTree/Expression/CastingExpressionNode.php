60b23fb41ee6e1b18319bd2e685a46609432ae4e
Claus Due <claus@namelesscoder.net>
2020-01-27 12:27:38 +0100

    [TASK] Add aliases for Expression node types to ViewHelpers
    Defines aliases for the removed Expression node types
    to redirect them to their new ViewHelper replacements.
    
    Allows configurations for Fluid 2.x to be used with 3.x
    without the need for changing configured class names.

---
+++ src/Core/Parser/SyntaxTree/Expression/CastingExpressionNode.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+namespace TYPO3Fluid\Fluid\Core\Parser\SyntaxTree\Expression;
+
+/*
+ * This file belongs to the package "TYPO3 Fluid".
+ * See LICENSE.txt that was shipped with this package.
+ */
+
+use TYPO3Fluid\Fluid\ViewHelpers\Expression\CastViewHelper;
+
+/**
+ * STUB CLASS: Casting Expression
+ *
+ * Allows configurations compatible with Fluid 2.x to also
+ * work transparently on Fluid 3.x by aliasing the built-in
+ * expression node types to their new ViewHelper replacements.
+ *
+ * @deprecated Will be removed in Fluid 4.0
+ */
+class CastingExpressionNode extends CastViewHelper
+{
+}
