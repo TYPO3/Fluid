421adfcdafaa6f19975a3a3d514c5c317359aeb8
Claus Due <claus@namelesscoder.net>
2020-01-27 12:16:41 +0100

    [TASK] Introduce stub classes for ViewHelper API compatibility
    Adds stubs for ViewHelperNode and TemplateCompiler
    to allow ViewHelpers written for 2.x to be statically
    analysed and validated without removing compile().

---
+++ src/Core/Parser/SyntaxTree/ViewHelperNode.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+namespace TYPO3Fluid\Fluid\Core\Parser\SyntaxTree;
+
+/*
+ * This file belongs to the package "TYPO3 Fluid".
+ * See LICENSE.txt that was shipped with this package.
+ */
+
+/**
+ * STUB CLASS: ViewHelperNode
+ *
+ * Exists solely to allow static analysis of ViewHelper classes
+ * which implement compile() to still be technically valid with
+ * Fluid 3.0.
+ *
+ * @deprecated Will be removed in Fluid 4.0
+ */
+class ViewHelperNode
+{
+}
