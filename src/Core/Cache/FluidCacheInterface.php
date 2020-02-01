bb0e33e0edf01797087d9001b48308b763e9eb19
Claus Due <claus@namelesscoder.net>
2020-02-01 12:53:59 +0100

    [TASK] Create stub for FluidCacheInterface
    Allows statically analysed classes which implement
    the Fluid caches from 2.x to still be technically valid
    when using Fluid 3.0.

---
+++ src/Core/Cache/FluidCacheInterface.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+namespace TYPO3Fluid\Fluid\Core\Cache;
+
+/*
+ * This file belongs to the package "TYPO3 Fluid".
+ * See LICENSE.txt that was shipped with this package.
+ */
+
+/**
+ * STUB CLASS: FluidCacheInterface
+ *
+ * Exists solely to allow static analysis of implementation classes
+ * for Fluid cache to still be technically valid with Fluid 3.0.
+ *
+ * @deprecated Will be removed in Fluid 4.0
+ */
+interface FluidCacheInterface
+{
+}
