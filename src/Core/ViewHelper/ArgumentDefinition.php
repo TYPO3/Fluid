06a34c8dc71666fdb3643a91056b82b325115306
Claus Due <claus@namelesscoder.net>
2020-02-01 13:28:40 +0100

    [TASK] Alias ArgumentDefinition from old class name
    Allows older Fluid implementations to use the
    ArgumentDefinition class with the old class name.

---
+++ src/Core/ViewHelper/ArgumentDefinition.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+namespace TYPO3Fluid\Fluid\Core\ViewHelper;
+
+/**
+ * Alias: ArgumentDefinition
+ *
+ * Allows older Fluid implementations to use the ArgumentDefinition
+ * class with the old class name.
+ *
+ * @deprecated Will be removed in Fluid 4.0
+ */
+class ArgumentDefinition extends \TYPO3Fluid\Fluid\Component\Argument\ArgumentDefinition
+{
+
+}
