89e785d80d5d8a444d4ad45db2a68cf24c09b4d3
Claus Due <claus@namelesscoder.net>
2020-02-01 13:59:58 +0100

    [TASK] Stub ArgumentCollection from Fluid 3.0 API
    Creates a stub for the new ArgumentCollection class which
    is added in Fluid 3.0. Allows Fluid 2.x implementations to
    be technically valid when performing static analysis on
    classes which implement API for both Fluid 2.x and 3.x.

---
+++ src/Component/Argument/ArgumentCollection.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+namespace TYPO3Fluid\Fluid\Component\Argument;
+
+/*
+ * This file belongs to the package "TYPO3 Fluid".
+ * See LICENSE.txt that was shipped with this package.
+ */
+
+/**
+ * STUB CLASS: ArgumentCollection
+ *
+ * Exists solely to allow static analysis of ViewHelper classes
+ * which implement new Fluid 3.x API to be technically valid.
+ */
+class ArgumentCollection
+{
+}
