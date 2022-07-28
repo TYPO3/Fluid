8c24c40f15d2fb5ddb2e166d007a57bb5a29bf23
Christian Kuhn <lolli@schwarzbu.ch>
2022-07-28 19:55:47 +0200

    [TASK] Update invalid annotations in ErrorHandlerInterface
    These @param annotations are obsolete since they
    are enforced by method signatures.
    
    > vendor/bin/phpstan --generate-baseline

--- src/Core/ErrorHandler/ErrorHandlerInterface.php
+++ src/Core/ErrorHandler/ErrorHandlerInterface.php
@@ -2,16 +2,12 @@
 
 namespace TYPO3Fluid\Fluid\Core\ErrorHandler;
 
-/**
- * Interface ErrorHandlerInterface
- */
 interface ErrorHandlerInterface
 {
     /**
      * Handle errors caused by parsing templates, for example when
      * invalid arguments are used.
      *
-     * @param \TYPO3Fluid\Fluid\Core\Parser\Exception $error
      * @return string
      */
     public function handleParserError(\TYPO3Fluid\Fluid\Core\Parser\Exception $error);
@@ -21,7 +17,6 @@ interface ErrorHandlerInterface
      * raised from misuse of `{variable xyz 123}` style expressions,
      * such as the casting expression `{variable as type}`.
      *
-     * @param \TYPO3Fluid\Fluid\Core\Parser\ExpressionException $error
      * @return string
      */
     public function handleExpressionError(\TYPO3Fluid\Fluid\Core\Parser\SyntaxTree\Expression\ExpressionException $error);
@@ -30,7 +25,6 @@ interface ErrorHandlerInterface
      * Can be implemented to handle a ViewHelper errors which are
      * normally thrown from inside ViewHelpers during rendering.
      *
-     * @param \TYPO3Fluid\Fluid\Core\ViewHelper\Exception $error
      * @return string
      */
     public function handleViewHelperError(\TYPO3Fluid\Fluid\Core\ViewHelper\Exception $error);
@@ -41,13 +35,11 @@ interface ErrorHandlerInterface
      * but if your application deems compiler errors fatal then
      * you can throw a different exception type here).
      *
-     * @param \TYPO3Fluid\Fluid\Core\Compiler\StopCompilingException $error
      * @return string
      */
     public function handleCompilerError(\TYPO3Fluid\Fluid\Core\Compiler\StopCompilingException $error);
 
     /**
-     * @param \TYPO3Fluid\Fluid\View\Exception $error
      * @return string
      */
     public function handleViewError(\TYPO3Fluid\Fluid\View\Exception $error);
