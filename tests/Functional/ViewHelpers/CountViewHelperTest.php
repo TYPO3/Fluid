881fd041a7b7085664c75fa188acb1e632bfa419
Anja Leichsenring <aleichsenring@ab-softlab.de>
2022-08-02 08:37:11 +0200

    [TASK] Refactor count VH tests to functionals
    In order to provide reliable testing scenarios for ViewHelpers,
    we prepare real world application of passing templates into
    the Fluid Parser and comparing the result over isolated
    variable passage into VH Helper methods.
    
    With the refactoring also caching can now be tested.
    
    All f:count tests have been refactored to functional tests
    with this patch.

---
+++ tests/Functional/ViewHelpers/CountViewHelperTest.php
@@ -0,0 +1,82 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file belongs to the package "TYPO3 Fluid".
+ * See LICENSE.txt that was shipped with this package.
+ */
+
+namespace TYPO3Fluid\Fluid\Tests\Functional\ViewHelpers;
+
+use TYPO3Fluid\Fluid\Tests\Functional\AbstractFunctionalTestCase;
+use TYPO3Fluid\Fluid\View\TemplateView;
+
+class CountViewHelperTest extends AbstractFunctionalTestCase
+{
+    /**
+     * @test
+     */
+    public function renderThrowsExceptionIfSubjectIsNotCountable()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $value = new \stdClass();
+        $view = new TemplateView();
+        $view->assignMultiple(['value' => $value]);
+        $view->getRenderingContext()->setCache(self::$cache);
+        $view->getRenderingContext()->getTemplatePaths()->setTemplateSource('<f:count subject="{value}" />');
+        $view->render();
+    }
+
+    public function renderDataProvider(): \Generator
+    {
+        yield 'value as argument' => [
+            '<f:count subject="{0:foo, 1:bar, 2:baz}" />',
+            [],
+            3,
+        ];
+        $value = new \ArrayObject(['foo', 'bar']);
+        yield 'value is object' => [
+            '<f:count subject="{value}" />',
+            ['value' => $value],
+            2,
+        ];
+        $value = [];
+        yield 'zero for empty array' => [
+            '<f:count subject="{value}" />',
+            ['value' => $value],
+            0,
+        ];
+        $value = ['foo', 'baz', 'bar'];
+        yield 'value as tag content' => [
+            '<f:count>{value}</f:count>',
+            ['value' => $value],
+            3,
+        ];
+        $value = [];
+        yield 'empty value as tag content' => [
+            '<f:count>{value}</f:count>',
+            ['value' => $value],
+            0,
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider renderDataProvider
+     */
+    public function render(string $template, array $variables, $expected): void
+    {
+        $view = new TemplateView();
+        $view->assignMultiple($variables);
+        $view->getRenderingContext()->setCache(self::$cache);
+        $view->getRenderingContext()->getTemplatePaths()->setTemplateSource($template);
+        self::assertSame($expected, $view->render());
+
+        $view = new TemplateView();
+        $view->assignMultiple($variables);
+        $view->getRenderingContext()->setCache(self::$cache);
+        $view->getRenderingContext()->getTemplatePaths()->setTemplateSource($template);
+        self::assertSame($expected, $view->render());
+    }
+}
