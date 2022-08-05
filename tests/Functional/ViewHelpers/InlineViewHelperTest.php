a024268f17a108bcde8263f23de2007ff7008ad1
Simon Praetorius <praetorius@sitegeist.de>
2022-08-06 00:21:27 +0200

    [TASK] Refactor inline VH tests to functionals (#635)
    In order to provide reliable testing scenarios for ViewHelpers,
    we prepare real world application of passing templates into
    the Fluid Parser and comparing the result over isolated
    variable passage into VH Helper methods.
    
    With the refactoring also caching can now be tested.
    
    All f:inline tests have been refactored to functional tests
    with this patch.

---
+++ tests/Functional/ViewHelpers/InlineViewHelperTest.php
@@ -0,0 +1,83 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file belongs to the package "TYPO3 Fluid".
+ * See LICENSE.txt that was shipped with this package.
+ */
+
+namespace TYPO3Fluid\Fluid\Tests\Functional\ViewHelpers;
+
+use TYPO3Fluid\Fluid\Core\Parser\Exception;
+use TYPO3Fluid\Fluid\Tests\Functional\AbstractFunctionalTestCase;
+use TYPO3Fluid\Fluid\View\TemplateView;
+
+class InlineViewHelperTest extends AbstractFunctionalTestCase
+{
+    /**
+     * @test
+     */
+    public function renderThrowsExceptionIfInlineFluidCodeIsInvalid()
+    {
+        $this->expectException(Exception::class);
+        $view = new TemplateView();
+        $view->assignMultiple(['code' => '<f:if condition="{undefinedVariable}">']);
+        $view->getRenderingContext()->setCache(self::$cache);
+        $view->getRenderingContext()->getTemplatePaths()->setTemplateSource('<f:inline code="{code}" />');
+        $view->render();
+    }
+
+    public function renderDataProvider(): \Generator
+    {
+        yield 'empty string' => [
+            '<f:inline code="{code}" />',
+            [
+                'code' => ''
+            ],
+            null,
+        ];
+        yield 'valid code with undefined variable' => [
+            '<f:inline code="{code}" />',
+            [
+                'code' => '{f:if(condition: undefinedVariable, then: \'foo\', else: \'bar\')}'
+            ],
+            'bar',
+        ];
+        yield 'valid code with defined variable' => [
+            '<f:inline code="{code}" />',
+            [
+                'code' => '{f:if(condition: definedVariable, then: \'foo\', else: \'bar\')}',
+                'definedVariable' => true
+            ],
+            'foo',
+        ];
+        yield 'tag content' => [
+            '<f:inline>{code}</f:inline>',
+            [
+                'code' => '{f:if(condition: definedVariable, then: \'foo\', else: \'bar\')}',
+                'definedVariable' => true
+            ],
+            'foo',
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider renderDataProvider
+     */
+    public function render(string $template, array $variables, $expected): void
+    {
+        $view = new TemplateView();
+        $view->assignMultiple($variables);
+        $view->getRenderingContext()->setCache(self::$cache);
+        $view->getRenderingContext()->getTemplatePaths()->setTemplateSource($template);
+        self::assertSame($expected, $view->render());
+
+        $view = new TemplateView();
+        $view->assignMultiple($variables);
+        $view->getRenderingContext()->setCache(self::$cache);
+        $view->getRenderingContext()->getTemplatePaths()->setTemplateSource($template);
+        self::assertSame($expected, $view->render());
+    }
+}
