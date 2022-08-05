80225ef2e22171dcb3ea606e5848ba3bb9a5ac11
Simon Praetorius <praetorius@sitegeist.de>
2022-08-05 22:00:14 +0200

    [TASK] Refactor section VH tests to functionals (#635)
    In order to provide reliable testing scenarios for ViewHelpers,
    we prepare real world application of passing templates into
    the Fluid Parser and comparing the result over isolated
    variable passage into VH Helper methods.
    
    With the refactoring also caching can now be tested.
    
    All f:section tests have been refactored to functional tests
    with this patch.

---
+++ tests/Functional/ViewHelpers/SectionViewHelperTest.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file belongs to the package "TYPO3 Fluid".
+ * See LICENSE.txt that was shipped with this package.
+ */
+
+namespace TYPO3Fluid\Fluid\Tests\Functional\ViewHelpers;
+
+use TYPO3Fluid\Fluid\Tests\Functional\AbstractFunctionalTestCase;
+use TYPO3Fluid\Fluid\View\TemplateView;
+
+class SectionViewHelperTest extends AbstractFunctionalTestCase
+{
+    public function renderDataProvider(): \Generator
+    {
+        yield 'section will not render itself' => [
+            '<f:section name="foo">bar</f:section>',
+            '', // @TODO inconsistency, in this case Fluid returns empty string, in other cases null
+        ];
+        yield 'render section without arguments before section was defined' => [
+            '<f:render section="foo" /><f:section name="foo">bar</f:section>',
+            'bar',
+        ];
+        yield 'render section without arguments after section was defined' => [
+            '<f:section name="foo">bar</f:section><f:render section="foo" />',
+            'bar',
+        ];
+        yield 'render section with arguments' => [
+            '<f:section name="foo">{var}</f:section><f:render section="foo" arguments="{var: \'bar\'}" />',
+            'bar',
+        ];
+        yield 'render section with arguments multiple times' => [
+            '<f:section name="foo">{var}</f:section>' .
+            '<f:render section="foo" arguments="{var: \'value1\'}" /> ' .
+            '<f:render section="foo" arguments="{var: \'value2\'}" />',
+            'value1 value2',
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider renderDataProvider
+     */
+    public function render(string $template, $expected): void
+    {
+        $view = new TemplateView();
+        $view->getRenderingContext()->setCache(self::$cache);
+        $view->getRenderingContext()->getTemplatePaths()->setTemplateSource($template);
+        self::assertSame($expected, $view->render());
+
+        $view = new TemplateView();
+        $view->getRenderingContext()->setCache(self::$cache);
+        $view->getRenderingContext()->getTemplatePaths()->setTemplateSource($template);
+        self::assertSame($expected, $view->render());
+    }
+}
