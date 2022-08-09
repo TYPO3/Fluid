a7f9c6ca25cd7d6ebdc7719d0b00f6cb3cb9c2bf
Anja <aleichsenring@ab-softlab.de>
2022-08-09 16:07:19 +0200

    [TASK] Refactor cache.disable VH tests to functionals (#648)
    In order to provide reliable testing scenarios for ViewHelpers,
    we prepare real world application of passing templates into
    the Fluid Parser and comparing the result over isolated
    variable passage into VH Helper methods.
    
    With the refactoring also caching can now be tested.
    
    All f:cache.disable tests have been refactored to functional tests
    with this patch.

---
+++ tests/Functional/ViewHelpers/Cache/DisableViewHelperTest.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file belongs to the package "TYPO3 Fluid".
+ * See LICENSE.txt that was shipped with this package.
+ */
+
+namespace TYPO3Fluid\Fluid\Tests\Functional\ViewHelpers\Cache;
+
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
+use TYPO3Fluid\Fluid\Tests\Functional\AbstractFunctionalTestCase;
+use TYPO3Fluid\Fluid\View\TemplateView;
+use TYPO3Fluid\Fluid\ViewHelpers\Cache\DisableViewHelper;
+
+class DisableViewHelperTest extends AbstractFunctionalTestCase
+{
+    /**
+     * @test
+     */
+    public function viewHelperCanBeInstantiated(): void
+    {
+        $subject = new DisableViewHelper();
+        self::assertInstanceOf(AbstractViewHelper::class, $subject);
+    }
+
+    /**
+     * @test
+     */
+    public function templateUsingViewHelperCanBeRendered(): void
+    {
+        $template = '<f:cache.disable>foo</f:cache.disable>';
+        $expected = 'foo';
+
+        $view = new TemplateView();
+        $view->getRenderingContext()->setCache(self::$cache);
+        $view->getRenderingContext()->getTemplatePaths()->setTemplateSource($template);
+        self::assertSame($expected, $view->render());
+
+        $view = new TemplateView();
+        $view->getRenderingContext()->setCache(self::$cache);
+        $view->getRenderingContext()->getTemplatePaths()->setTemplateSource($template);
+        self::assertSame($expected, $view->render());
+    }
+}
