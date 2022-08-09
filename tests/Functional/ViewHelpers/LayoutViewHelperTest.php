3f8aca0b03fe07efa8fca04c1927b49d8d2675dd
Anja <aleichsenring@ab-softlab.de>
2022-08-09 15:59:57 +0200

    [TASK] Refactor layout VH tests to functionals (#647)
    In order to provide reliable testing scenarios for ViewHelpers,
    we prepare real world application of passing templates into
    the Fluid Parser and comparing the result over isolated
    variable passage into VH Helper methods.
    
    With the refactoring also caching can now be tested.
    
    All f:layout tests have been refactored to functional tests
    with this patch.

---
+++ tests/Functional/ViewHelpers/LayoutViewHelperTest.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file belongs to the package "TYPO3 Fluid".
+ * See LICENSE.txt that was shipped with this package.
+ */
+
+namespace TYPO3Fluid\Fluid\Tests\Functional\ViewHelpers;
+
+use TYPO3Fluid\Fluid\Tests\Functional\AbstractFunctionalTestCase;
+use TYPO3Fluid\Fluid\View\TemplateView;
+
+class LayoutViewHelperTest extends AbstractFunctionalTestCase
+{
+    public function renderDataProvider(): \Generator
+    {
+        yield 'template with named layout' => [
+            '<f:layout name="Layout" /><f:section name="main">Content of main section</f:section>',
+            'DefaultLayoutContent of main section',
+        ];
+        yield 'fallback layout' => [
+            '<f:layout />',
+            'DefaultLayout with name Default.html',
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider renderDataProvider
+     */
+    public function render(string $template, string $expected): void
+    {
+        $view = new TemplateView();
+        $view->getRenderingContext()->setCache(self::$cache);
+        $view->getRenderingContext()->getTemplatePaths()->setLayoutRootPaths([__DIR__ . '/../Fixtures/Layouts/']);
+        $view->getRenderingContext()->getTemplatePaths()->setTemplateSource($template);
+        self::assertSame($expected, trim($view->render()));
+
+        $view = new TemplateView();
+        $view->getRenderingContext()->setCache(self::$cache);
+        $view->getRenderingContext()->getTemplatePaths()->setLayoutRootPaths([__DIR__ . '/../Fixtures/Layouts/']);
+        $view->getRenderingContext()->getTemplatePaths()->setTemplateSource($template);
+        self::assertSame($expected, trim($view->render()));
+    }
+}
