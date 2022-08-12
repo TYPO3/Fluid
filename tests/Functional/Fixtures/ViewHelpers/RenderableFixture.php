d0038c517204763678be31a031bcb21b183d1d37
Anja <aleichsenring@ab-softlab.de>
2022-08-12 12:27:50 +0200

    [TASK] Refactor render VH tests to functionals (#649)
    In order to provide reliable testing scenarios for ViewHelpers,
    we prepare real world application of passing templates into
    the Fluid Parser and comparing the result over isolated
    variable passage into VH Helper methods.
    
    With the refactoring also caching can now be tested.
    
    All f:render tests have been refactored to functional tests
    with this patch.

---
+++ tests/Functional/Fixtures/ViewHelpers/RenderableFixture.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file belongs to the package "TYPO3 Fluid".
+ * See LICENSE.txt that was shipped with this package.
+ */
+
+namespace TYPO3Fluid\Fluid\Tests\Functional\Fixtures\ViewHelpers;
+
+use TYPO3Fluid\Fluid\Core\Parser\SyntaxTree\NodeInterface;
+use TYPO3Fluid\Fluid\Core\Parser\SyntaxTree\TextNode;
+use TYPO3Fluid\Fluid\Core\Rendering\RenderableInterface;
+use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
+
+class RenderableFixture implements RenderableInterface
+{
+    public function getName()
+    {
+        return 'RenderableFixture';
+    }
+
+    public function setName($name)
+    {
+        // stub
+    }
+
+    public function setNode(NodeInterface $node)
+    {
+        // stub
+    }
+
+    public function getNode()
+    {
+        return new TextNode(sprintf('%s (%s)', static::class, 'RenderableFixture'));
+    }
+
+    public function render(RenderingContextInterface $renderingContext): string
+    {
+        return 'rendered by renderable';
+    }
+}
