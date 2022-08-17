da48e515ae26c0e5ac148b1006e6f543fda4fcc6
Anja Leichsenring <aleichsenring@ab-softlab.de>
2022-08-17 11:28:16 +0200

    [WIP][TASK] Provide proper PHP formatting for executable

---
+++ bin/fluid.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file belongs to the package "TYPO3 Fluid".
+ * See LICENSE.txt that was shipped with this package.
+ */
+
+use TYPO3Fluid\Fluid\Tools\Console\ConsoleRunner;
+
+if (file_exists(__DIR__ . '/../autoload.php')) {
+    require_once __DIR__ . '/../autoload.php';
+} elseif (file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    require_once __DIR__ . '/../vendor/autoload.php';
+} elseif (file_exists(__DIR__ . '/../../../autoload.php')) {
+    require_once __DIR__ . '/../../../autoload.php';
+}
+
+$runner = new ConsoleRunner();
+try {
+    echo $runner->handleCommand($argv);
+} catch (InvalidArgumentException $error) {
+    echo PHP_EOL . 'ERROR! ' . $error->getMessage() . PHP_EOL . PHP_EOL;
+    echo $runner->dumpSupportedParameters();
+}
