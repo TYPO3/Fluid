4b666b02b1c2435fb6466a4f7494dd6a53722ec4
Anja Leichsenring <aleichsenring@ab-softlab.de>
2022-08-26 10:52:16 +0200

    [TASK] Move calling file into better position

--- src/Tools/console.php
+++ src/Tools/console.php
@@ -9,12 +9,12 @@ declare(strict_types=1);
 
 use TYPO3Fluid\Fluid\Tools\ConsoleRunner;
 
-if (file_exists(__DIR__ . '/../autoload.php')) {
-    require_once __DIR__ . '/../autoload.php';
-} elseif (file_exists(__DIR__ . '/../vendor/autoload.php')) {
-    require_once __DIR__ . '/../vendor/autoload.php';
-} elseif (file_exists(__DIR__ . '/../../../autoload.php')) {
-    require_once __DIR__ . '/../../../autoload.php';
+if (file_exists(__DIR__ . '/../../autoload.php')) {
+    require_once __DIR__ . '/../../autoload.php';
+} elseif (file_exists(__DIR__ . '/../../vendor/autoload.php')) {
+    require_once __DIR__ . '/../../vendor/autoload.php';
+} elseif (file_exists(__DIR__ . '/../../../../autoload.php')) {
+    require_once __DIR__ . '/../../../../autoload.php';
 }
 
 $runner = new ConsoleRunner();
