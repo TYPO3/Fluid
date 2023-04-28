4c8d91564320b256e982ad16ff7a79c0c9203dae
Christian Eßl <christian.essl@cyberhouse.at>
2023-04-28 15:11:02 +0200

    [TASK] Strict comparison on string compares
    Resolves: #713

--- src/Core/Parser/SyntaxTree/Expression/TernaryExpressionNode.php
+++ src/Core/Parser/SyntaxTree/Expression/TernaryExpressionNode.php
@@ -93,7 +93,7 @@ class TernaryExpressionNode extends AbstractExpressionNode
         $context = [];
         if (preg_match_all(static::$variableDetection, $expression, $matches) > 0) {
             foreach ($matches[1] as $variable) {
-                if (strtolower($variable) == 'true' || strtolower($variable) == 'false' || empty($variable) === true) {
+                if (strtolower($variable) === 'true' || strtolower($variable) === 'false' || empty($variable) === true) {
                     continue;
                 }
                 $context[$variable] = static::getTemplateVariableOrValueItself($variable, $renderingContext);
