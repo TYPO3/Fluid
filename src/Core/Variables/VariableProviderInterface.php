912fb510c8c5837ab4236b91cbfc1da88b00e01e
Christian Kuhn <lolli@schwarzbu.ch>
2023-05-03 08:33:23 +0200

    [TASK] Simplify VariableProviderInterface->getByPath()
    The second argument $accessors can be removed
    from VariableProviderInterface since it fits
    no purpose anymore with a1645c53 being merged.
    
    The patch removes the argument from the interface
    only, keeping consumers as is for now, to verify
    this change is not breaking.

--- src/Core/Variables/VariableProviderInterface.php
+++ src/Core/Variables/VariableProviderInterface.php
@@ -94,15 +94,11 @@ interface VariableProviderInterface extends \ArrayAccess
     /**
      * Get a variable by dotted path expression, retrieving the
      * variable from nested arrays/objects one segment at a time.
-     * If the second variable is passed, it is expected to contain
-     * extraction method names (constants from StandardVariableProvider)
-     * which indicate how each value is extracted.
      *
      * @param string $path
-     * @param array $accessors
      * @return mixed
      */
-    public function getByPath($path, array $accessors = []);
+    public function getByPath($path);
 
     /**
      * Remove a variable from context.
