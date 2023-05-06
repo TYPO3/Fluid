81684d82bf12b7a24af1c4531a35c1e699a2086c
Christian Kuhn <lolli@schwarzbu.ch>
2023-05-06 14:33:50 +0200

    [TASK] Avoid TestCase::getMockForTrait()
    phpunit 10.1 soft deprecated getMockForTrait().
    We can avoid usages by adding a fixture class
    that uses the trait.

--- tests/Unit/Core/ViewHelper/Traits/ParserRuntimeOnlyTest.php
+++ tests/Unit/Core/ViewHelper/Traits/ParserRuntimeOnlyTest.php
@@ -11,7 +11,7 @@ namespace TYPO3Fluid\Fluid\Tests\Unit\Core\ViewHelper\Traits;
 
 use TYPO3Fluid\Fluid\Core\Compiler\TemplateCompiler;
 use TYPO3Fluid\Fluid\Core\Parser\SyntaxTree\ViewHelperNode;
-use TYPO3Fluid\Fluid\Core\ViewHelper\Traits\ParserRuntimeOnly;
+use TYPO3Fluid\Fluid\Tests\Unit\Core\ViewHelper\Traits\Fixtures\ParserRuntimeOnlyFixture;
 use TYPO3Fluid\Fluid\Tests\UnitTestCase;
 
 class ParserRuntimeOnlyTest extends UnitTestCase
@@ -19,22 +19,19 @@ class ParserRuntimeOnlyTest extends UnitTestCase
     /**
      * @test
      */
-    public function testRenderReturnsNull(): void
+    public function renderReturnsNull(): void
     {
-        $instance = $this->getMockBuilder(ParserRuntimeOnly::class)->getMockForTrait();
-        $result = $instance->render();
-        self::assertNull($result);
+        self::assertNull((new ParserRuntimeOnlyFixture())->render());
     }
 
     /**
      * @test
      */
-    public function testCompileReturnsEmptyString(): void
+    public function compileReturnsEmptyString(): void
     {
-        $trait = $this->getMockBuilder(ParserRuntimeOnly::class)->getMockForTrait();
-        $init = '';
-        $viewHelperNodeMock = $this->getMock(ViewHelperNode::class, [], [], false, false);
-        $result = $trait->compile('fake', 'fake', $init, $viewHelperNodeMock, new TemplateCompiler());
+        $initializationPhpCode = '';
+        $subject = new ParserRuntimeOnlyFixture();
+        $result = $subject->compile('', '', $initializationPhpCode, $this->createMock(ViewHelperNode::class), new TemplateCompiler());
         self::assertSame('', $result);
     }
 }
