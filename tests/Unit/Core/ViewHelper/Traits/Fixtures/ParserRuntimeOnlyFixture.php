f22d07d1cf76769e39c77b292044ce0f1e4e553c
Christian Kuhn <lolli@schwarzbu.ch>
2023-05-06 14:39:30 +0200

    [TASK] Avoid TestCase::getMockForTrait() (#743)
    phpunit 10.1 soft deprecated getMockForTrait().
    We can avoid usages by adding a fixture class
    that uses the trait.

---
+++ tests/Unit/Core/ViewHelper/Traits/Fixtures/ParserRuntimeOnlyFixture.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * This file belongs to the package "TYPO3 Fluid".
+ * See LICENSE.txt that was shipped with this package.
+ */
+
+namespace TYPO3Fluid\Fluid\Tests\Unit\Core\ViewHelper\Traits\Fixtures;
+
+use TYPO3Fluid\Fluid\Core\ViewHelper\Traits\ParserRuntimeOnly;
+
+class ParserRuntimeOnlyFixture
+{
+    use ParserRuntimeOnly;
+}
