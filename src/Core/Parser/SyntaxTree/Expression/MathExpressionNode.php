271becfdff579bea91e84e9ae0aae53440db3f10
Thomas Lüder <t.lueder@nets.de>
2023-05-11 18:31:18 +0200

    Cast $result to float or int  just once

--- src/Core/Parser/SyntaxTree/Expression/MathExpressionNode.php
+++ src/Core/Parser/SyntaxTree/Expression/MathExpressionNode.php
@@ -51,6 +51,7 @@ class MathExpressionNode extends AbstractExpressionNode
         // them in order.
         $result = array_shift($matches[0]);
         $result = static::getTemplateVariableOrValueItself($result, $renderingContext);
+        $result = ($result == (int)$result) ? (int)$result : (float)$result;
         $operator = null;
         $operators = ['*', '^', '-', '+', '/', '%'];
         foreach ($matches[0] as $part) {
@@ -59,7 +60,6 @@ class MathExpressionNode extends AbstractExpressionNode
             } else {
                 $part = static::getTemplateVariableOrValueItself($part, $renderingContext);
                 $part = ($part == (int)$part) ? (int)$part : (float)$part;
-                $result = ($result == (int)$result) ? (int)$result : (float)$result;
                 $result = self::evaluateOperation($result, $operator, $part);
             }
         }
