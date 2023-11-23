bf4c65a84850801fbf571cf19368c4a5e91472a0
Simon Praetorius <simon@praetorius.me>
2023-11-23 22:09:18 +0100

    [FEATURE] Nl2brViewHelper for StandaloneFluid (#838)
    Nl2brViewHelper doesn't have any dependencies to TYPO3, so it can be moved to Fluid Standalone.

---
+++ tests/Functional/ViewHelpers/Format/Nl2brViewHelperTest.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file belongs to the package "TYPO3 Fluid".
+ * See LICENSE.txt that was shipped with this package.
+ */
+
+namespace TYPO3Fluid\Fluid\Tests\Functional\ViewHelpers\Format;
+
+use TYPO3Fluid\Fluid\Tests\Functional\AbstractFunctionalTestCase;
+use TYPO3Fluid\Fluid\View\TemplateView;
+
+final class Nl2brViewHelperTest extends AbstractFunctionalTestCase
+{
+    public static function renderDataProvider(): array
+    {
+        return [
+            'viewHelperDoesNotModifyTextWithoutLineBreaks' => [
+                '<f:format.nl2br><p class="bodytext">Some Text without line breaks</p></f:format.nl2br>',
+                '<p class="bodytext">Some Text without line breaks</p>',
+            ],
+            'viewHelperConvertsLineBreaksToBRTags' => [
+                '<f:format.nl2br>' . 'Line 1' . chr(10) . 'Line 2' . '</f:format.nl2br>',
+                'Line 1<br />' . chr(10) . 'Line 2',
+            ],
+            'viewHelperConvertsWindowsLineBreaksToBRTags' => [
+                '<f:format.nl2br>' . 'Line 1' . chr(13) . chr(10) . 'Line 2' . '</f:format.nl2br>',
+                'Line 1<br />' . chr(13) . chr(10) . 'Line 2',
+            ],
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider renderDataProvider
+     */
+    public function render(string $template, string $expected): void
+    {
+        $view = new TemplateView();
+        $view->getRenderingContext()->setCache(self::$cache);
+        $view->getRenderingContext()->getTemplatePaths()->setTemplateSource($template);
+        self::assertSame($expected, $view->render());
+
+        $view = new TemplateView();
+        $view->getRenderingContext()->setCache(self::$cache);
+        $view->getRenderingContext()->getTemplatePaths()->setTemplateSource($template);
+        self::assertSame($expected, $view->render());
+    }
+}
