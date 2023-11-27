a78d64201f94502b27c351e37968636943257d52
Simon Praetorius <simon@praetorius.me>
2023-11-27 18:06:34 +0100

    [FEATURE] SplitViewHelper
    The SplitViewHelper splits a string by the specified separator, which
    results in an array. The number of values in the resulting array can
    be limited with the limit parameter, which results in an array where
    the last item contains the remaining unsplit string.
    
    This ViewHelper mimicks PHP's :php:`explode()` function.
    
    Split with a separator:
    
    ```xml
    <f:split value="1,5,8" separator="," /> <!-- Output: {0: '1', 1: '5', 2: '8'} -->
    ```
    
    Split using tag content as value:
    
    ```xml
    <f:split separator="-">1-5-8</f:split> <!-- Output: {0: '1', 1: '5', 2: '8'} -->
    ```
    
    Split with a limit:
    
    ```xml
    <f:split value="1,5,8" separator="," limit="2" /> <!-- Output: {0: '1', 1: '5,8'} -->
    ```

---
+++ tests/Functional/ViewHelpers/SplitViewHelperTest.php
@@ -0,0 +1,96 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file belongs to the package "TYPO3 Fluid".
+ * See LICENSE.txt that was shipped with this package.
+ */
+
+namespace TYPO3Fluid\Fluid\Tests\Functional\ViewHelpers;
+
+use TYPO3Fluid\Fluid\Tests\Functional\AbstractFunctionalTestCase;
+use TYPO3Fluid\Fluid\View\TemplateView;
+
+final class SplitViewHelperTest extends AbstractFunctionalTestCase
+{
+    /**
+     * @test
+     */
+    public function throwsExceptionForInvalidValue(): void
+    {
+        self::expectExceptionCode(1705250408);
+        $view = new TemplateView();
+        $view->getRenderingContext()->setCache(self::$cache);
+        $view->getRenderingContext()->getTemplatePaths()->setTemplateSource('<f:split separator="," />');
+        $view->render();
+    }
+
+    public static function renderDataProvider(): \Generator
+    {
+        yield 'value as argument' => [
+            '<f:split value="{value}" separator="," />',
+            ['value' => 'foo,bar,4'],
+            ['foo', 'bar', '4'],
+        ];
+        yield 'value as tag content' => [
+            '<f:split separator=",">{value}</f:split>',
+            ['value' => 'foo,bar,4'],
+            ['foo', 'bar', '4'],
+        ];
+        yield 'limit items' => [
+            '<f:split separator="." limit="2">{value}</f:split>',
+            ['value' => 'foo.bar.baz'],
+            ['foo', 'bar.baz'],
+        ];
+        yield 'limit matches result' => [
+            '<f:split separator="," limit="3">{value}</f:split>',
+            ['value' => 'foo,bar,baz'],
+            ['foo', 'bar', 'baz'],
+        ];
+        yield 'limit exceeds result' => [
+            '<f:split separator="," limit="10">{value}</f:split>',
+            ['value' => 'foo,bar,baz'],
+            ['foo', 'bar', 'baz'],
+        ];
+        yield 'limit zero' => [
+            '<f:split separator="," limit="0">{value}</f:split>',
+            ['value' => 'foo,bar,baz'],
+            ['foo,bar,baz'],
+        ];
+        yield 'limit one' => [
+            '<f:split separator="," limit="1">{value}</f:split>',
+            ['value' => 'foo,bar,baz'],
+            ['foo,bar,baz'],
+        ];
+        yield 'negative limit' => [
+            '<f:split separator="," limit="-1">{value}</f:split>',
+            ['value' => 'foo,bar,baz'],
+            ['foo', 'bar'],
+        ];
+        yield 'exceeding negative limit' => [
+            '<f:split separator="," limit="-5">{value}</f:split>',
+            ['value' => 'foo,bar,baz'],
+            [],
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider renderDataProvider
+     */
+    public function render(string $template, array $variables, $expected): void
+    {
+        $view = new TemplateView();
+        $view->assignMultiple($variables);
+        $view->getRenderingContext()->setCache(self::$cache);
+        $view->getRenderingContext()->getTemplatePaths()->setTemplateSource($template);
+        self::assertSame($expected, $view->render());
+
+        $view = new TemplateView();
+        $view->assignMultiple($variables);
+        $view->getRenderingContext()->setCache(self::$cache);
+        $view->getRenderingContext()->getTemplatePaths()->setTemplateSource($template);
+        self::assertSame($expected, $view->render());
+    }
+}
