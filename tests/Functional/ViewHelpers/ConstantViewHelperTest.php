6ddcfab0c178240a70b1a1cdd9545bd45a9f53a2
Andreas Kienast <andreas.kienast@typo3.com>
2024-06-24 19:04:34 +0200

    [FEATURE] Add f:constant ViewHelper (#874)
    This commit adds the `f:constant` ViewHelper allowing to use native PHP
    constants and enums in Fluid context.
    
    Fixes: #873

---
+++ tests/Functional/ViewHelpers/ConstantViewHelperTest.php
@@ -0,0 +1,112 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file belongs to the package "TYPO3 Fluid".
+ * See LICENSE.txt that was shipped with this package.
+ */
+
+namespace TYPO3Fluid\Fluid\Tests\Functional\ViewHelpers;
+
+use TYPO3Fluid\Fluid\Tests\Functional\AbstractFunctionalTestCase;
+use TYPO3Fluid\Fluid\Tests\Functional\Fixtures\Various\BackedEnumExample;
+use TYPO3Fluid\Fluid\Tests\Functional\Fixtures\Various\ClassConstantsExample;
+use TYPO3Fluid\Fluid\Tests\Functional\Fixtures\Various\EnumExample;
+use TYPO3Fluid\Fluid\View\TemplateView;
+
+final class ConstantViewHelperTest extends AbstractFunctionalTestCase
+{
+    /**
+     * @test
+     */
+    public function renderThrowsExceptionOnNonStringValue(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $name = new \stdClass();
+        $view = new TemplateView();
+        $view->assignMultiple(['name' => $name]);
+        $view->getRenderingContext()->setCache(self::$cache);
+        $view->getRenderingContext()->getTemplatePaths()->setTemplateSource('<f:constant name="{name}" />');
+        $view->render();
+    }
+
+    /**
+     * @test
+     */
+    public function renderThrowsErrorOnUndefinedConstant(): void
+    {
+        $this->expectException(\Error::class);
+        $name = 'FOO';
+        $view = new TemplateView();
+        $view->assignMultiple(['name' => $name]);
+        $view->getRenderingContext()->setCache(self::$cache);
+        $view->getRenderingContext()->getTemplatePaths()->setTemplateSource('<f:constant name="{name}" />');
+        $view->render();
+    }
+
+    public static function renderDataProvider(): \Generator
+    {
+        yield 'Name is built-in PHP constant' => [
+            'PHP_INT_MAX',
+            PHP_INT_MAX,
+        ];
+
+        yield 'Name is class constant w/out leading slash' => [
+            'TYPO3Fluid\Fluid\Tests\Functional\Fixtures\Various\ClassConstantsExample::FOO',
+            ClassConstantsExample::FOO,
+        ];
+
+        yield 'Name is class constant with leading slash' => [
+            '\TYPO3Fluid\Fluid\Tests\Functional\Fixtures\Various\ClassConstantsExample::FOO',
+            ClassConstantsExample::FOO,
+        ];
+
+        yield 'Name is backed enum case w/out leading slash' => [
+            'TYPO3Fluid\Fluid\Tests\Functional\Fixtures\Various\BackedEnumExample::BAR',
+            BackedEnumExample::BAR,
+        ];
+
+        yield 'Name is backed enum case with leading slash' => [
+            '\TYPO3Fluid\Fluid\Tests\Functional\Fixtures\Various\BackedEnumExample::BAR',
+            BackedEnumExample::BAR,
+        ];
+
+        yield 'Name is enum case w/out leading slash' => [
+            'TYPO3Fluid\Fluid\Tests\Functional\Fixtures\Various\EnumExample::FOO',
+            EnumExample::FOO,
+        ];
+
+        yield 'Name is enum case with leading slash' => [
+            '\TYPO3Fluid\Fluid\Tests\Functional\Fixtures\Various\EnumExample::FOO',
+            EnumExample::FOO,
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider renderDataProvider
+     */
+    public function render(mixed $name, mixed $expected): void
+    {
+        $templateSources = [
+            '<f:constant name="{name}" />',
+            '<f:constant>{name}</f:constant>',
+            '{f:constant(name: \'{name}\')}',
+        ];
+
+        foreach ($templateSources as $templateSource) {
+            $view = new TemplateView();
+            $view->assignMultiple(['name' => $name]);
+            $view->getRenderingContext()->setCache(self::$cache);
+            $view->getRenderingContext()->getTemplatePaths()->setTemplateSource($templateSource);
+            self::assertSame($expected, $view->render());
+
+            $view = new TemplateView();
+            $view->assignMultiple(['name' => $name]);
+            $view->getRenderingContext()->setCache(self::$cache);
+            $view->getRenderingContext()->getTemplatePaths()->setTemplateSource($templateSource);
+            self::assertSame($expected, $view->render());
+        }
+    }
+}
