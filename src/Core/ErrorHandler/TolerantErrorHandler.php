5fb23000d6cbb505e97402e5e7319490fa63601c
Simon Praetorius <simon@praetorius.me>
2024-07-21 18:50:57 +0200

    [TASK] Apply type hints to error handlers

--- src/Core/ErrorHandler/TolerantErrorHandler.php
+++ src/Core/ErrorHandler/TolerantErrorHandler.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 /*
  * This file belongs to the package "TYPO3 Fluid".
  * See LICENSE.txt that was shipped with this package.
@@ -19,47 +21,27 @@ namespace TYPO3Fluid\Fluid\Core\ErrorHandler;
  */
 class TolerantErrorHandler implements ErrorHandlerInterface
 {
-    /**
-     * @param \TYPO3Fluid\Fluid\Core\Parser\Exception $error
-     * @return string
-     */
-    public function handleParserError(\TYPO3Fluid\Fluid\Core\Parser\Exception $error)
+    public function handleParserError(\TYPO3Fluid\Fluid\Core\Parser\Exception $error): string
     {
         return 'Parser error: ' . $error->getMessage() . ' Offending code: ';
     }
 
-    /**
-     * @param \TYPO3Fluid\Fluid\Core\Parser\SyntaxTree\Expression\ExpressionException $error
-     * @return string
-     */
-    public function handleExpressionError(\TYPO3Fluid\Fluid\Core\Parser\SyntaxTree\Expression\ExpressionException $error)
+    public function handleExpressionError(\TYPO3Fluid\Fluid\Core\Parser\SyntaxTree\Expression\ExpressionException $error): string
     {
         return 'Invalid expression: ' . $error->getMessage();
     }
 
-    /**
-     * @param \TYPO3Fluid\Fluid\Core\ViewHelper\Exception $error
-     * @return string
-     */
-    public function handleViewHelperError(\TYPO3Fluid\Fluid\Core\ViewHelper\Exception $error)
+    public function handleViewHelperError(\TYPO3Fluid\Fluid\Core\ViewHelper\Exception $error): string
     {
         return 'ViewHelper error: ' . $error->getMessage() . ' - Offending code: ';
     }
 
-    /**
-     * @param \TYPO3Fluid\Fluid\Core\Compiler\StopCompilingException $error
-     * @return string
-     */
-    public function handleCompilerError(\TYPO3Fluid\Fluid\Core\Compiler\StopCompilingException $error)
+    public function handleCompilerError(\TYPO3Fluid\Fluid\Core\Compiler\StopCompilingException $error): string
     {
         return '';
     }
 
-    /**
-     * @param \TYPO3Fluid\Fluid\View\Exception $error
-     * @return string
-     */
-    public function handleViewError(\TYPO3Fluid\Fluid\View\Exception $error)
+    public function handleViewError(\TYPO3Fluid\Fluid\View\Exception $error): string
     {
         if ($error instanceof \TYPO3Fluid\Fluid\View\Exception\InvalidSectionException) {
             return 'Section rendering error: ' . $error->getMessage() . ' Section rendering is mandatory; "optional" is false.';
