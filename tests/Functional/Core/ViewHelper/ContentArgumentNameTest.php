1552904737e3d987d94768a613772c5c93d3df04
Simon Praetorius <simon@praetorius.me>
2024-08-14 15:01:56 +0200

    [BUGFIX] Respect getContentArgumentName() in TemplateParser (#968)
    With the introduction of the new `getContentArgumentName()`
    API for ViewHelpers, there has been an oversight in `TemplateParser`:
    It had special handling for the old method name
    `resolveContentArgumentName()`, which is now deprecated. Since
    `resolveContentArgumentName()` is now called by the new API
    `getContentArgumentName()`, we can just call the new API in the
    `TemplateParser` now, without special handling for the deprecated
    trait.
    
    As a consequence, we need to adjust some tests and fixtures. Also,
    we add more dedicated test cases to our new test file.
    
    In the future, there needs to be a cleanup of `AbstractViewHelper`
    and `ViewHelperInterface`, since internal classes depend on API
    only defined in `AbstractViewHelper`. In fact, we arguably made things
    a bit worse now, but this is a topic for Fluid v5.
    # Conflicts:
    #	phpstan-baseline.neon
    #	src/Core/ViewHelper/Traits/CompileWithContentArgumentAndRenderStatic.php

--- tests/Functional/Core/ViewHelper/ContentArgumentNameTest.php
+++ tests/Functional/Core/ViewHelper/ContentArgumentNameTest.php
@@ -19,26 +19,61 @@ final class ContentArgumentNameTest extends AbstractFunctionalTestCase
     public static function renderDataProvider(): array
     {
         return [
-            'self-closing tag without argument value' => ['<test:contentArgumentName />', null],
-            'only children' => ['<test:contentArgumentName>child value</test:contentArgumentName>', 'child value'],
-            'children and empty argument value' => ['<test:contentArgumentName value="">child value</test:contentArgumentName>', ''],
-            'children and argument value' => ['<test:contentArgumentName value="argument value">child value</test:contentArgumentName>', 'argument value'],
-            'self-closing tag with argument value' => ['<test:contentArgumentName value="argument value" />', 'argument value'],
-            'empty children and argument value' => ['<test:contentArgumentName value="argument value"></test:contentArgumentName>', 'argument value'],
+            'self-closing tag without argument value' => ['<test:contentArgumentName />', [], null],
+            'only children' => ['<test:contentArgumentName>child value</test:contentArgumentName>', [], 'child value'],
+            'children and empty argument value' => ['<test:contentArgumentName value="">child value</test:contentArgumentName>', [], ''],
+            'children and argument value' => ['<test:contentArgumentName value="argument value">child value</test:contentArgumentName>', [], 'argument value'],
+            'self-closing tag with argument value' => ['<test:contentArgumentName value="argument value" />', [], 'argument value'],
+            'empty children and argument value' => ['<test:contentArgumentName value="argument value"></test:contentArgumentName>', [], 'argument value'],
+
+            // These cases test special handling of content argument in TemplateParser::isArgumentEscaped()
+            'children with HTML to be escaped' => [
+                '<test:contentArgumentName><span class="test">test</span></test:contentArgumentName>',
+                [],
+                '&lt;span class=&quot;test&quot;&gt;test&lt;/span&gt;',
+            ],
+            'children with variable to be escaped' => [
+                '<test:contentArgumentName>{myVar}</test:contentArgumentName>',
+                ['myVar' => '<span class="test">test</span>'],
+                '&lt;span class=&quot;test&quot;&gt;test&lt;/span&gt;',
+            ],
+            'children with HTML not to be escaped' => [
+                '<test:contentArgumentNameWithoutEscapeChildren><span class="test">test</span></test:contentArgumentNameWithoutEscapeChildren>',
+                [],
+                '&lt;span class=&quot;test&quot;&gt;test&lt;/span&gt;',
+            ],
+            'children with variable not to be escaped' => [
+                '<test:contentArgumentNameWithoutEscapeChildren>{myVar}</test:contentArgumentNameWithoutEscapeChildren>',
+                ['myVar' => '<span class="test">test</span>'],
+                '&lt;span class=&quot;test&quot;&gt;test&lt;/span&gt;',
+            ],
+            'argument with variable to be escaped' => [
+                '<test:contentArgumentName value="{myVar}" />',
+                ['myVar' => '<span class="test">test</span>'],
+                '&lt;span class=&quot;test&quot;&gt;test&lt;/span&gt;',
+            ],
+            'argument with variable not to be escaped' => [
+                '<test:contentArgumentNameWithoutEscapeChildren value="{myVar}" />',
+                ['myVar' => '<span class="test">test</span>'],
+                '&lt;span class=&quot;test&quot;&gt;test&lt;/span&gt;',
+            ],
         ];
     }
 
     #[DataProvider('renderDataProvider')]
     #[Test]
-    public function render(string $source, ?string $expected): void
+    public function render(string $source, array $variables, ?string $expected): void
     {
         $view = new TemplateView();
         $view->getRenderingContext()->getViewHelperResolver()->addNamespace('test', 'TYPO3Fluid\\Fluid\\Tests\\Functional\\Fixtures\\ViewHelpers');
+        $view->assignMultiple($variables);
         $view->getRenderingContext()->setCache(self::$cache);
         $view->getRenderingContext()->getTemplatePaths()->setTemplateSource($source);
         self::assertSame($expected, $view->render());
 
         $view = new TemplateView();
+        $view->getRenderingContext()->getViewHelperResolver()->addNamespace('test', 'TYPO3Fluid\\Fluid\\Tests\\Functional\\Fixtures\\ViewHelpers');
+        $view->assignMultiple($variables);
         $view->getRenderingContext()->setCache(self::$cache);
         $view->getRenderingContext()->getTemplatePaths()->setTemplateSource($source);
         self::assertSame($expected, $view->render());
