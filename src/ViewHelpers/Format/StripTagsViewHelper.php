4c2876f0d6eb2d0c9cb4a54f5b5004131786b940
Tiran Grigoryan <t.grigoryan@agentur-fahrenheit.de>
2025-04-25 19:55:35 +0200

    [FEATURE] Add support for `hrefsInBrackets` in StripTagsViewHelper #102651
    Allow `<a>` tag href links to be appended in square brackets next to link text when `hrefsInBrackets` is enabled. Updated tests and documentation to reflect the new functionality.

--- src/ViewHelpers/Format/StripTagsViewHelper.php
+++ src/ViewHelpers/Format/StripTagsViewHelper.php
@@ -10,6 +10,7 @@ declare(strict_types=1);
 namespace TYPO3Fluid\Fluid\ViewHelpers\Format;
 
 use Stringable;
+use TYPO3\CMS\Extbase\Utility\DebuggerUtility;
 use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
 
 /**
@@ -44,6 +45,19 @@ use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
  *
  *    <p>paragraph</p><span>span</span><div>divider</div>iframe<script>script</script>
  *
+ * Default notation with hrefsInBrackets
+ * ---------------------------------
+ *
+ * ::
+ *
+ *    <f:format.stripTags hrefsInBrackets="true">
+ *        <a href="https://example.com">Link Text</a>
+ *    </f:format.stripTags>
+ *
+ * Output::
+ *
+ *    "Link Text [https://example.com]"
+ *
  * Inline notation
  * ---------------
  *
@@ -61,6 +75,16 @@ use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
  *    {text -> f:format.stripTags(allowedTags: "<p><span><div><script>")}
  *
  * Text with p, span, div and script Tags inside, all other tags are removed.
+ *
+ * Inline notation with hrefsInBrackets
+ * --------------------------------
+ *
+ * ::
+ *
+ *    {text -> f:format.stripTags(hrefsInBrackets: "true")}
+ *
+ * Href links in Text in square brackets next to the link text, e.g. "Link Text [https://example.com]".
+ *
  */
 final class StripTagsViewHelper extends AbstractViewHelper
 {
@@ -75,6 +99,11 @@ final class StripTagsViewHelper extends AbstractViewHelper
     {
         $this->registerArgument('value', 'string', 'string to format');
         $this->registerArgument('allowedTags', 'string', 'Optional string of allowed tags as required by PHPs strip_tags() function');
+        $this->registerArgument(
+            'hrefsInBrackets',
+            'bool',
+            'When enabled, detects <a> tags in the content and appends their href links in square brackets next to the link text, e.g. "Link Text [https://example.com]"'
+        );
     }
 
     /**
@@ -94,11 +123,30 @@ final class StripTagsViewHelper extends AbstractViewHelper
     {
         $value = $this->renderChildren();
         $allowedTags = $this->arguments['allowedTags'];
+        $hrefsInBrackets = $this->arguments['hrefsInBrackets'];
         if (is_array($value)) {
             throw new \InvalidArgumentException('Specified array cannot be converted to string.', 1700819707);
         }
         if (is_object($value) && !($value instanceof Stringable)) {
             throw new \InvalidArgumentException('Specified object cannot be converted to string.', 1700819706);
+        }
+        if ($hrefsInBrackets === true) {
+            $value = preg_replace_callback(
+                '/<a\s+(?:[^>]*?\s+)?href=(["\'])(.*?)\1[^>]*>(.*?)<\/a>/i',
+                function($matches) {
+                    if (count($matches) >= 4) {
+                        $url = $matches[2];
+                        $text = $matches[3];
+                        if (strlen($url) > 0 && filter_var($url, FILTER_VALIDATE_URL)) {
+                            return '"' . $text . ' [' . htmlspecialchars($url, ENT_QUOTES, 'UTF-8') . ']"';
+                        } else if (strlen($text) > 0) {
+                            return $text;
+                        } else {
+                            return '';
+                        }
+                    }
+                }, $value
+            );
         }
         return strip_tags((string)$value, $allowedTags);
     }
