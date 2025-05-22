3ad10c56e28e44caea0f106a877ff71d518c1cb3
metapublic GbR <31284546+metapublic-gbr@users.noreply.github.com>
2025-05-22 13:33:31 +0200

    Update TrimViewHelper.php
    Add inline example

--- src/ViewHelpers/Format/TrimViewHelper.php
+++ src/ViewHelpers/Format/TrimViewHelper.php
@@ -63,6 +63,14 @@ use TYPO3Fluid\Fluid\Core\ViewHelper\Exception;
  * .. code-block:: text
  *
  *    #ring to be trimmed#
+ *
+ *
+ * Inline usage
+ * -----------------------
+ *
+ * ::
+ *
+ *    #{f:format.trim(value: '{my_variable}')}#
  */
 final class TrimViewHelper extends AbstractViewHelper
 {
