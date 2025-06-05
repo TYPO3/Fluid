fecabca7f1eb4b0fb90904e53b23186ba94f26ec
Simon Praetorius <simon@praetorius.me>
2025-06-05 16:58:22 +0200

    [TASK] Add more test coverage for inline ViewHelper syntax

---
+++ tests/Functional/Core/Parser/ViewHelperNamespacesTest.php
@@ -0,0 +1,129 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file belongs to the package "TYPO3 Fluid".
+ * See LICENSE.txt that was shipped with this package.
+ */
+
+namespace TYPO3Fluid\Fluid\Tests\Functional\Core\Parser;
+
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\Test;
+use TYPO3Fluid\Fluid\Tests\Functional\AbstractFunctionalTestCase;
+use TYPO3Fluid\Fluid\View\TemplateView;
+
+final class ViewHelperNamespacesTest extends AbstractFunctionalTestCase
+{
+    public static function getTemplateCodeFixturesAndExpectations(): array
+    {
+        return [
+            'Ignoring namespaces without conflict with registered namespace' => [
+                '{namespace z*}{namespace bar}<zoo:bar /><bar:foo></bar:foo><zoo.bar:baz /><f:format.raw>foobar</f:format.raw>',
+                '<zoo:bar /><bar:foo></bar:foo><zoo.bar:baz />foobar',
+            ],
+            'Ignoring namespaces with conflict with registered namespace gives registered namespace priority' => [
+                '{namespace f*}{namespace bar}<foo:bar /><bar:foo></bar:foo><foo.bar:baz /><f:format.raw>foobar</f:format.raw>',
+                '<foo:bar /><bar:foo></bar:foo><foo.bar:baz />foobar',
+            ],
+        ];
+    }
+
+    #[DataProvider('getTemplateCodeFixturesAndExpectations')]
+    #[Test]
+    public function testTemplateCodeFixture(string $source, string $expectedInOutput): void
+    {
+        $view = new TemplateView();
+        $view->getRenderingContext()->setCache(self::$cache);
+        $view->getRenderingContext()->getTemplatePaths()->setTemplateSource($source);
+        $output = $view->render();
+        self::assertSame($expectedInOutput, $output);
+
+        // Second run to verify cached behavior.
+        $view = new TemplateView();
+        $view->getRenderingContext()->setCache(self::$cache);
+        $view->getRenderingContext()->getTemplatePaths()->setTemplateSource($source);
+        $output = $view->render();
+        self::assertSame($expectedInOutput, $output);
+    }
+
+    public static function inlineViewHelperSyntaxDataProvider(): array
+    {
+        return [
+            'Single inline ViewHelper' => [
+                '{test:contentArgumentName(value: input)}',
+                'input',
+            ],
+            'Single inline ViewHelper, content argument' => [
+                '{input -> test:contentArgumentName()}',
+                'input',
+            ],
+            'Two inline ViewHelpers' => [
+                '{test:contentArgumentName(value: input) -> f:format.json()}',
+                '&quot;input&quot;',
+            ],
+            'Two inline ViewHelpers, content argument' => [
+                '{input -> test:contentArgumentName() -> f:format.json()}',
+                '&quot;input&quot;',
+            ],
+            'Multiple inline ViewHelpers' => [
+                '{test:contentArgumentName(value: input) -> f:format.json() -> f:format.raw()}',
+                '"input"',
+            ],
+            'Multiple inline ViewHelpers, content argument' => [
+                '{input -> test:contentArgumentName() -> f:format.json() -> f:format.raw()}',
+                '"input"',
+            ],
+
+            'Single inline ViewHelper, nested input variable' => [
+                '{test:contentArgumentName(value: nestedInput.0)}',
+                'input',
+            ],
+            'Single inline ViewHelper, content argument, nested input variable' => [
+                '{nestedInput.0 -> test:contentArgumentName()}',
+                'input',
+            ],
+            'Two inline ViewHelpers, nested input variable' => [
+                '{test:contentArgumentName(value: nestedInput.0) -> f:format.json()}',
+                '&quot;input&quot;',
+            ],
+            'Two inline ViewHelpers, content argument, nested input variable' => [
+                '{nestedInput.0 -> test:contentArgumentName() -> f:format.json()}',
+                '&quot;input&quot;',
+            ],
+            'Multiple inline ViewHelpers, nested input variable' => [
+                '{test:contentArgumentName(value: nestedInput.0) -> f:format.json() -> f:format.raw()}',
+                '"input"',
+            ],
+            'Multiple inline ViewHelpers, content argument, nested input variable' => [
+                '{nestedInput.0 -> test:contentArgumentName() -> f:format.json() -> f:format.raw()}',
+                '"input"',
+            ],
+        ];
+    }
+
+    #[Test]
+    #[DataProvider('inlineViewHelperSyntaxDataProvider')]
+    public function inlineViewHelperSyntax(string $source, string $expected): void
+    {
+        $view = new TemplateView();
+        $view->assign('input', 'input');
+        $view->assign('nestedInput', ['input']);
+        $view->getRenderingContext()->setCache(self::$cache);
+        $view->getRenderingContext()->getViewHelperResolver()->addNamespace('test', 'TYPO3Fluid\Fluid\Tests\Functional\Fixtures\ViewHelpers');
+        $view->getRenderingContext()->getTemplatePaths()->setTemplateSource($source);
+        $output = $view->render();
+        self::assertSame($expected, $output);
+
+        // Second run to verify cached behavior.
+        $view = new TemplateView();
+        $view->assign('input', 'input');
+        $view->assign('nestedInput', ['input']);
+        $view->getRenderingContext()->setCache(self::$cache);
+        $view->getRenderingContext()->getViewHelperResolver()->addNamespace('test', 'TYPO3Fluid\Fluid\Tests\Functional\Fixtures\ViewHelpers');
+        $view->getRenderingContext()->getTemplatePaths()->setTemplateSource($source);
+        $output = $view->render();
+        self::assertSame($expected, $output);
+    }
+}
