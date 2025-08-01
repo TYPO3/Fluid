a60ae8376adf848c67236ef6c18a9dafae4f08c4
Simon Praetorius <simon@praetorius.me>
2025-08-01 16:07:02 +0200

    [!!!][TASK] Remove renderStatic() for ViewHelpers
    `renderStatic()` and the associated traits have been deprecated with
    Fluid v2.15 and are now removed.

--- tests/Unit/Core/ViewHelper/AbstractViewHelperTest.php
+++ tests/Unit/Core/ViewHelper/AbstractViewHelperTest.php
@@ -21,8 +21,6 @@ use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
 use TYPO3Fluid\Fluid\Core\ViewHelper\ArgumentDefinition;
 use TYPO3Fluid\Fluid\Core\ViewHelper\Exception;
 use TYPO3Fluid\Fluid\Core\ViewHelper\ViewHelperVariableContainer;
-use TYPO3Fluid\Fluid\Tests\Unit\Core\ViewHelper\Fixtures\RenderMethodFreeDefaultRenderStaticViewHelper;
-use TYPO3Fluid\Fluid\Tests\Unit\Core\ViewHelper\Fixtures\RenderMethodFreeViewHelper;
 
 final class AbstractViewHelperTest extends TestCase
 {
@@ -143,26 +141,5 @@ final class AbstractViewHelperTest extends TestCase
         $result = $subject->compile('foobar', 'baz', $init, $node, new TemplateCompiler());
         self::assertEmpty($init);
         self::assertEquals('$renderingContext->getViewHelperInvoker()->invoke(' . get_class($subject) . '::class, foobar, $renderingContext, baz)', $result);
-    }
-
-    #[Test]
-    #[IgnoreDeprecations]
-    public function testCallRenderMethodCanRenderViewHelperWithoutRenderMethodAndCallsRenderStatic(): void
-    {
-        $subject = new RenderMethodFreeViewHelper();
-        $method = new \ReflectionMethod($subject, 'render');
-        $subject->setRenderingContext(new RenderingContext());
-        $result = $method->invoke($subject);
-        self::assertSame('I was rendered', $result);
-    }
-
-    #[Test]
-    public function testCallRenderMethodOnViewHelperWithoutRenderMethodWithDefaultRenderStaticMethodThrowsException(): void
-    {
-        $this->expectException(Exception::class);
-        $subject = new RenderMethodFreeDefaultRenderStaticViewHelper();
-        $method = new \ReflectionMethod($subject, 'render');
-        $subject->setRenderingContext(new RenderingContext());
-        $method->invoke($subject);
     }
 }
