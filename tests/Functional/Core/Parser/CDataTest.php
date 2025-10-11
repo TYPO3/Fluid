a922631ee102e6d10f9cc4c25e1901d0bec6fcfb
Simon Praetorius <simon@praetorius.me>
2025-10-11 18:21:20 +0200

    [TASK] Extend tests

--- tests/Functional/Core/Parser/CDataTest.php
+++ tests/Functional/Core/Parser/CDataTest.php
@@ -9,6 +9,7 @@ declare(strict_types=1);
 
 namespace TYPO3Fluid\Fluid\Tests\Functional\Cases\Parsing;
 
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\IgnoreDeprecations;
 use PHPUnit\Framework\Attributes\Test;
 use TYPO3Fluid\Fluid\Tests\Functional\AbstractFunctionalTestCase;
@@ -16,22 +17,64 @@ use TYPO3Fluid\Fluid\View\TemplateView;
 
 final class CDataTest extends AbstractFunctionalTestCase
 {
-    #[Test]
-    #[IgnoreDeprecations]
-    public function cDataRemovedFromTemplate(): void
+    public static function handleCdataInTemplateDataProvider(): array
     {
-        $source = 'some content <![CDATA[some content within cdata]]> more content';
-        $expected = 'some content  more content';
+        return [
+            'simple string' => [
+                'some content <![CDATA[some content within cdata]]> more content',
+                [],
+                'some content  more content',
+            ],
 
+            'normal variable syntax' => [
+                'some content <![CDATA[some {content} within cdata]]> more content',
+                ['content' => 'foo'],
+                'some content  more content',
+            ],
+            'normal ViewHelper tag syntax' => [
+                'some content <![CDATA[<f:format.trim>  some content within cdata  </f:format.trim>]]> more content',
+                [],
+                'some content  more content',
+            ],
+            'normal ViewHelper inline syntax' => [
+                'some content <![CDATA[{f:format.trim(value: \'  some content within cdata  \')}]]> more content',
+                [],
+                'some content  more content',
+            ],
+            'normal math syntax' => [
+                'some content <![CDATA[{1 + 2}]]> more content',
+                [],
+                'some content  more content',
+            ],
+            'normal cast syntax' => [
+                'some content <![CDATA[{var as float}]]> more content',
+                ['var' => 1.0],
+                'some content  more content',
+            ],
+            'normal ternary syntax' => [
+                'some content <![CDATA[{var1 ? var2 : var3}]]> more content',
+                ['var1' => 'foo', 'var2' => 'bar', 'var3' => 'baz'],
+                'some content  more content',
+            ],
+        ];
+    }
+
+    #[Test]
+    #[DataProvider('handleCdataInTemplateDataProvider')]
+    #[IgnoreDeprecations]
+    public function handleCdataInTemplate(string $template, array $variables, string $expected): void
+    {
         $view = new TemplateView();
         $view->getRenderingContext()->setCache(self::$cache);
-        $view->getRenderingContext()->getTemplatePaths()->setTemplateSource($source);
+        $view->assignMultiple($variables);
+        $view->getRenderingContext()->getTemplatePaths()->setTemplateSource($template);
         $output = $view->render();
         self::assertSame($expected, $output);
 
         $view = new TemplateView();
         $view->getRenderingContext()->setCache(self::$cache);
-        $view->getRenderingContext()->getTemplatePaths()->setTemplateSource($source);
+        $view->assignMultiple($variables);
+        $view->getRenderingContext()->getTemplatePaths()->setTemplateSource($template);
         $output = $view->render();
         self::assertSame($expected, $output);
     }
