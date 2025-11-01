b57955b941c913dadeb3d1dd6e9c3b3916cfb401
Elias Häußler <elias@haeussler.dev>
2025-11-01 11:01:39 +0100

    [TASK] Remove a duplicate header comment

--- src/Exception.php
+++ src/Exception.php
@@ -7,11 +7,6 @@
 
 namespace TYPO3Fluid\Fluid;
 
-/*
- * This file belongs to the package "TYPO3 Fluid".
- * See LICENSE.txt that was shipped with this package.
- */
-
 /**
  * The most general Fluid exception.
  *
